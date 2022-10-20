eed2c2b5e2454915247eface8c854c1918472b1a
BruceMD <bruce.dickie@jembi.org>
2022-10-20 15:02:52 +0200

    Scaling up Prom

--- docker/helper/scripts/d-stack-03-up-hub-containers.sh
+++ docker/helper/scripts/d-stack-03-up-hub-containers.sh
@@ -6,28 +6,29 @@ set -u
 #trap '' INT
 
 pushd .
-  SCRIPT_DIR=$(cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P)
-  cd ${SCRIPT_DIR}/../..
+SCRIPT_DIR=$(cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P)
+cd ${SCRIPT_DIR}/../..
 
-  source ./0-conf.env
+source ./0-conf.env
 
-  # docker service scale ${STACK_NAME}_cassandra-1=${SCALE_CASSANDRA_1}
-  # docker service scale ${STACK_NAME}_cassandra-2=${SCALE_CASSANDRA_2}
-  # docker service scale ${STACK_NAME}_cassandra-3=${SCALE_CASSANDRA_3}
-  docker service scale ${STACK_NAME}_kafka-1=${SCALE_KAFKA_1}
-  docker service scale ${STACK_NAME}_kafka-2=${SCALE_KAFKA_2}
-  docker service scale ${STACK_NAME}_kafka-3=${SCALE_KAFKA_3}
-  docker service scale ${STACK_NAME}_lambda=${SCALE_LAMBDA}
-  docker service scale ${STACK_NAME}_zero=${SCALE_ZERO}
-  docker service scale ${STACK_NAME}_alpha1=${SCALE_ALPHA1}
-  docker service scale ${STACK_NAME}_alpha2=${SCALE_ALPHA2}
-  docker service scale ${STACK_NAME}_alpha3=${SCALE_ALPHA3}
-  docker service scale ${STACK_NAME}_ratel=${SCALE_RATEL}
+# docker service scale ${STACK_NAME}_cassandra-1=${SCALE_CASSANDRA_1}
+# docker service scale ${STACK_NAME}_cassandra-2=${SCALE_CASSANDRA_2}
+# docker service scale ${STACK_NAME}_cassandra-3=${SCALE_CASSANDRA_3}
+docker service scale ${STACK_NAME}_kafka-1=${SCALE_KAFKA_1}
+docker service scale ${STACK_NAME}_kafka-2=${SCALE_KAFKA_2}
+docker service scale ${STACK_NAME}_kafka-3=${SCALE_KAFKA_3}
+docker service scale ${STACK_NAME}_lambda=${SCALE_LAMBDA}
+docker service scale ${STACK_NAME}_zero=${SCALE_ZERO}
+docker service scale ${STACK_NAME}_alpha1=${SCALE_ALPHA1}
+docker service scale ${STACK_NAME}_alpha2=${SCALE_ALPHA2}
+docker service scale ${STACK_NAME}_alpha3=${SCALE_ALPHA3}
+docker service scale ${STACK_NAME}_ratel=${SCALE_RATEL}
+docker service scale ${STACK_NAME}_prom=${SCALE_PROMETHEUS}
 
-  pushd helper/topics
+pushd helper/topics
 #   ./topics-delete.sh
-    ./topics-create.sh
-    ./topics-list.sh
-  popd
+./topics-create.sh
+./topics-list.sh
+popd
 
 popd
