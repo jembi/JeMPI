addd1b6440bd417507b1e77c011812b2abc479d7
Bruce Dickie <bruce.dickie@jembi.org>
2022-11-16 08:24:27 +0200

    Remove echo statements
    They were there for progress tracking

--- docker/z-stack-2-reboot.sh
+++ docker/z-stack-2-reboot.sh
@@ -12,14 +12,14 @@ echo Down stack
 
 echo
 echo Up app containers
-echo CREATE DIRS
+
 ./helper/scripts/d-stack-01-create-dirs.sh
 sleep 2
-echo DEPLOY
+
 ./helper/scripts/d-stack-02-deploy-0.sh
 sleep 2
-echo UP HUB CONTAINERS
+
 ./helper/scripts/d-stack-03-up-hub-containers.sh
 sleep 2
-echo UP APP CONTAINERS
+
 ./helper/scripts/d-stack-04-up-app-containers.sh
