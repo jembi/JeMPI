3c955ccf488f18eed43083339db317d889245363
Matthew Erispe <matthew.erispe@jembi.org>
2022-11-18 14:04:49 +0200

    JMPI-97 update jenkins configuration settings and script

--- docker/d-jenkins-1-create.sh
+++ docker/d-jenkins-1-create.sh
@@ -6,24 +6,22 @@ set -u
 source ./0-conf.env
 source ./conf/images/conf-hub-images.sh
 
-#sudo rm -r -f ${PWD}/jenkins_data
-mkdir -p ${PWD}/jenkins_data
+mkdir -p ${PWD}/jenkins_docker_client
 
-#echo "PLACEMENT_REGISTRY: ${PLACEMENT_REGISTRY}"
+mkdir -p ${PWD}/jenkins_data
+sudo chmod 777 ${PWD}/jenkins_data
 
 sudo chmod 666 /var/run/docker.sock
-sudo cp /usr/bin/docker ${PWD}/jenkins_docker_client
+sudo cp /usr/bin/docker ${PWD}/jenkins_docker_client/
 
 echo $PWD
 
-#docker service rm jenkins
-
 docker service create \
   --name jenkins \
-  --publish published=80,target=8080,protocol=tcp,mode=host \
+  --publish published=8088,target=8080,protocol=tcp,mode=host \
   --mount type=bind,source=${PWD}/jenkins_data,destination=/bitnami/jenkins,readonly=false \
   --mount type=bind,source=${PWD}/jenkins_docker_client/docker,destination=/usr/bin/docker,readonly=true \
   --mount type=bind,source=/var/run/docker.sock,destination=/var/run/docker.sock \
   --mount type=bind,source=/bin/envsubst,destination=/bin/envsubst \
-  --constraint node.hostname==getafix \
-bitnami/jenkins:2
+  --constraint node.hostname==${PLACEMENT_REGISTRY} \
+${JENKINS_IMAGE}
