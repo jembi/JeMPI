919a2c8de78789a0d8b3a889539d0afeaafc4d27
William Hayes <william.hayes@jembi.org>
2022-11-23 01:17:26 +0200

    clean up

---
+++ docker/conf/env/create-env-linux-1.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+export PROJECT_DIR=$(builtin cd ../../; pwd)
+export PROJECT_DATA_DIR=${PROJECT_DIR}/docker_data/data
+export PROJECT_DATA_APPS_DIR=${PROJECT_DIR}/docker_data/data-apps
+export PROJECT_DATA_MONITOR_DIR=${PROJECT_DIR}/docker_data/data-monitor
+
+export NODE1=$(hostname)
+export NODE1_IP=$(hostname -i)
+
+export SCALE_KAFKA_01=1
+export SCALE_KAFKA_02=1
+export SCALE_KAFKA_03=1
+export SCALE_ZERO_01=1
+export SCALE_ALPHA_01=1
+export SCALE_ALPHA_02=1
+export SCALE_ALPHA_03=1
+export SCALE_RATEL=1
+
+
+# DON'T CHANGE
+export REGISTRY_NODE_IP=${NODE1}:5000/v2
+
+envsubst < conf-env-1-pc.template > conf.env
