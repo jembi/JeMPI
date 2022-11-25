352f3ae3a0bc7ffd03a44a4ed37604454d188ab5
Ryan Crichton <ryan@jembi.org>
2022-11-25 16:52:06 +0200

    Add get document API scaffolding

--- JeMPI_Apps/JeMPI_LibMPI/src/main/java/org/jembi/jempi/libmpi/dgraph/LibDgraph.java
+++ JeMPI_Apps/JeMPI_LibMPI/src/main/java/org/jembi/jempi/libmpi/dgraph/LibDgraph.java
@@ -18,133 +18,142 @@ import static io.dgraph.DgraphProto.Operation.DropOp.DATA;
 
 public class LibDgraph implements LibMPIClientInterface {
 
-   private static final Logger LOGGER = LogManager.getLogger(LibDgraph.class);
+    private static final Logger LOGGER = LogManager.getLogger(LibDgraph.class);
 
-   public LibDgraph(final String[] host, final int[] port) {
-      LOGGER.info("{}", "LibDgraph Constructor");
+    public LibDgraph(final String[] host, final int[] port) {
+        LOGGER.info("{}", "LibDgraph Constructor");
 
-      Client.getInstance().config(host, port);
-   }
+        Client.getInstance().config(host, port);
+    }
 
+    /*
+     * *******************************************************
+     * QUERIES
+     * *******************************************************
+     *
+     */
 
-   /*
-    * *******************************************************
-    *   QUERIES
-    * *******************************************************
-    *
-    */
+    public List<CustomGoldenRecord> getCandidates(final CustomEntity customEntity,
+            final boolean applyDeterministicFilter) {
+        // final var dgraphEntity = new CustomLibMPIDGraphEntity(mpiEntity.entity(),
+        // mpiEntity.score());
+        final var candidates = CustomLibMPIQueries.getCandidates(customEntity, applyDeterministicFilter);
+        return candidates.stream().map(CustomLibMPIGoldenRecord::toCustomGoldenRecord).toList();
+    }
 
-   public List<CustomGoldenRecord> getCandidates(final CustomEntity customEntity, final boolean applyDeterministicFilter) {
-//      final var dgraphEntity = new CustomLibMPIDGraphEntity(mpiEntity.entity(), mpiEntity.score());
-      final var candidates = CustomLibMPIQueries.getCandidates(customEntity, applyDeterministicFilter);
-      return candidates.stream().map(CustomLibMPIGoldenRecord::toCustomGoldenRecord).toList();
-   }
+    public List<MpiExpandedGoldenRecord> getMpiExpandedGoldenRecordList(final List<String> goldenIdList) {
+        final var list = Queries.getExpandedGoldenRecordList(goldenIdList);
+        return list.stream().map(CustomLibMPIExpandedGoldenRecord::toMpiExpandedGoldenRecord).toList();
+    }
 
-   public List<MpiExpandedGoldenRecord> getMpiExpandedGoldenRecordList(final List<String> goldenIdList) {
-      final var list = Queries.getExpandedGoldenRecordList(goldenIdList);
-      return list.stream().map(CustomLibMPIExpandedGoldenRecord::toMpiExpandedGoldenRecord).toList();
-   }
+    public List<String> getGoldenIdListByPredicate(final String predicate, final String val) {
+        return Queries.getGoldenIdListByPredicate(predicate, val);
+    }
 
-   public List<String> getGoldenIdListByPredicate(final String predicate, final String val) {
-      return Queries.getGoldenIdListByPredicate(predicate, val);
-   }
+    public CustomGoldenRecord getGoldenRecordByUid(final String uid) {
+        final var rec = Queries.getGoldenRecordByUid(uid);
+        if (rec == null) {
+            return null;
+        }
+        return rec.toCustomGoldenRecord();
+    }
 
-   public CustomGoldenRecord getGoldenRecordByUid(final String uid) {
-      final var rec = Queries.getGoldenRecordByUid(uid);
-      if (rec == null) {
-         return null;
-      }
-      return rec.toCustomGoldenRecord();
-   }
+    public CustomEntity getMpiEntity(final String uid) {
+        final var customEntity = Queries.getDGraphEntity(uid);
+        return customEntity;
+    }
 
-   public CustomEntity getMpiEntity(final String uid) {
-      final var customEntity = Queries.getDGraphEntity(uid);
-      return customEntity;
-   }
+    public List<String> getGoldenIdList() {
+        return Queries.getGoldenIdList();
+    }
 
-   public List<String> getGoldenIdList() {
-      return Queries.getGoldenIdList();
-   }
+    @Override
+    public CustomEntity getDocument(String uid) {
+        final var rec = Queries.getDGraphEntity(uid);
+        if (rec == null) {
+            return null;
+        }
+        return rec;
+    }
 
-   public long countGoldenRecords() {
-      return Queries.countGoldenRecords();
-   }
+    public long countGoldenRecords() {
+        return Queries.countGoldenRecords();
+    }
 
-   public long countEntities() {
-      return Queries.countEntities();
-   }
+    public long countEntities() {
+        return Queries.countEntities();
+    }
 
+    /*
+     * *******************************************************
+     * MUTATIONS
+     * *******************************************************
+     */
 
-   /*
-    * *******************************************************
-    *   MUTATIONS
-    * *******************************************************
-    */
+    public boolean updateGoldenRecordPredicate(final String uid, final String predicate, final String val) {
+        final var rc = Mutations.updateGoldenRecordPredicate(uid, predicate, val);
+        return rc;
+    }
 
-   public boolean updateGoldenRecordPredicate(final String uid, final String predicate, final String val) {
-      final var rc = Mutations.updateGoldenRecordPredicate(uid, predicate, val);
-      return rc;
-   }
+    public Either<MpiGeneralError, LinkInfo> unLink(final String goldenID, final String entityID, final float score) {
+        return Mutations.unLink(goldenID, entityID, score);
+    }
 
-   public Either<MpiGeneralError, LinkInfo> unLink(final String goldenID, final String entityID, final float score) {
-      return Mutations.unLink(goldenID, entityID, score);
-   }
+    public Either<MpiGeneralError, LinkInfo> updateLink(final String goldenID, final String newGoldenID,
+            final String entityID,
+            final float score) {
+        return Mutations.updateLink(goldenID, newGoldenID, entityID, score);
+    }
 
-   public Either<MpiGeneralError, LinkInfo> updateLink(final String goldenID, final String newGoldenID, final String entityID,
-                                                       final float score) {
-      return Mutations.updateLink(goldenID, newGoldenID, entityID, score);
-   }
+    public LinkInfo createEntityAndLinkToExistingGoldenRecord(final CustomEntity customEntity,
+            final GoldenIdScore goldenIdScore) {
+        // final var dgraphEntity = new CustomLibMPIDGraphEntity(customEntity,
+        // goldenIdScore);
+        return Mutations.linkDGraphEntity(customEntity, goldenIdScore);
+    }
 
-   public LinkInfo createEntityAndLinkToExistingGoldenRecord(final CustomEntity customEntity,
-                                                             final GoldenIdScore goldenIdScore) {
-//      final var dgraphEntity = new CustomLibMPIDGraphEntity(customEntity, goldenIdScore);
-      return Mutations.linkDGraphEntity(customEntity, goldenIdScore);
-   }
+    public LinkInfo createEntityAndLinkToClonedGoldenRecord(final CustomEntity customEntity, float score) {
+        final var dgraphEntity = new CustomLibMPIDGraphEntity(customEntity, score);
+        final var linkInfo = Mutations.addNewDGraphEntity(customEntity);
+        return linkInfo;
+    }
 
-   public LinkInfo createEntityAndLinkToClonedGoldenRecord(final CustomEntity customEntity, float score) {
-      final var dgraphEntity = new CustomLibMPIDGraphEntity(customEntity, score);
-      final var linkInfo = Mutations.addNewDGraphEntity(customEntity);
-      return linkInfo;
-   }
+    public void startTransaction() {
+        Client.getInstance().startTransaction();
+    }
 
-   public void startTransaction() {
-      Client.getInstance().startTransaction();
-   }
+    public void closeTransaction() {
+        Client.getInstance().closeTransaction();
+    }
 
-   public void closeTransaction() {
-      Client.getInstance().closeTransaction();
-   }
+    /*
+     * *******************************************************
+     * DATABASE
+     * *******************************************************
+     */
 
+    public Option<MpiGeneralError> dropAll() {
+        try {
+            Client.getInstance().alter(DgraphProto.Operation.newBuilder().setDropAll(true).build());
+            return Option.none();
+        } catch (RuntimeException e) {
+            LOGGER.error(e.getMessage(), e);
+            return Option.of(new MpiServiceError.GeneralError("Drop All Error"));
+        }
+    }
 
+    public Option<MpiGeneralError> dropAllData() {
+        try {
+            Client.getInstance().alter(DgraphProto.Operation.newBuilder().setDropOp(DATA).build());
+            return Option.none();
+        } catch (RuntimeException e) {
+            LOGGER.error(e.getMessage());
+            return Option.of(new MpiServiceError.GeneralError("Drop All Data Error"));
+        }
+    }
 
-   /*
-    * *******************************************************
-    *   DATABASE
-    * *******************************************************
-    */
-
-   public Option<MpiGeneralError> dropAll() {
-      try {
-         Client.getInstance().alter(DgraphProto.Operation.newBuilder().setDropAll(true).build());
-         return Option.none();
-      } catch (RuntimeException e) {
-         LOGGER.error(e.getMessage(), e);
-         return Option.of(new MpiServiceError.GeneralError("Drop All Error"));
-      }
-   }
-
-   public Option<MpiGeneralError> dropAllData() {
-      try {
-         Client.getInstance().alter(DgraphProto.Operation.newBuilder().setDropOp(DATA).build());
-         return Option.none();
-      } catch (RuntimeException e) {
-         LOGGER.error(e.getMessage());
-         return Option.of(new MpiServiceError.GeneralError("Drop All Data Error"));
-      }
-   }
-
-   public Option<MpiGeneralError> createSchema() {
-      return Mutations.createSchema();
-   }
+    public Option<MpiGeneralError> createSchema() {
+        return Mutations.createSchema();
+    }
 
 }
