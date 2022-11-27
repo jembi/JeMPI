9421d0e22b4e95d0c87241e9947f22eea99e19c5
William Hayes <william.hayes@jembi.org>
2022-11-27 11:30:08 +0200

    Add fhir_async/sync senders templates

---
+++ JeMPI_Apps/JeMPI_FHIRsyncSender/src/main/java/org/jembi/jempi/fhir_sync_sender/Main.java
@@ -0,0 +1,7 @@
+package org.jembi.jempi.fhir_sync_sender;
+
+public class Main {
+   public static void main(String[] args) {
+      new CustomSender().run();
+   }
+}
---
+++ JeMPI_Apps/JeMPI_Shared_Source/custom/reference/fhir_sync_sender/CustomSender.java
@@ -0,0 +1,7 @@
+package org.jembi.jempi.fhir_sync_sender;
+
+class CustomSender {
+   public void run() {
+      System.out.println("Hello world!");
+   }
+}
