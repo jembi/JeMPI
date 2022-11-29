3d22bfdef44797a764b38c2f61ed3882b3d1c434
mahaomolise92 <nahaomolise92@gmail.com>
2022-11-29 15:01:11 +0200

    sql updates

--- JeMPI_Apps/JeMPI_API/src/main/java/org/jembi/jempi/shared/models/Notification.java
+++ JeMPI_Apps/JeMPI_API/src/main/java/org/jembi/jempi/shared/models/Notification.java
@@ -1,1 +1,26 @@
-../../../../../../../../../JeMPI_Shared_Source/models/Notification.java
+package org.jembi.jempi.shared.models;
+
+import java.util.List;
+
+public record Notification(Long timeStamp,  // UTC Time
+                           NotificationType notificationType,
+                           String dID,   // Document ID
+                           List<String> patientNames,
+                           MatchData linkedTo,
+                           List<MatchData> candidates) {
+    public enum NotificationType {
+        THRESHOLD("Threshold"),
+        MARGIN("Margin");
+
+        public final String label;
+
+        private NotificationType(String label) {
+            this.label = label;
+        }
+    }
+
+    public record MatchData(String gID,  // Golden ID
+                            Float score) {
+
+    }
+}
