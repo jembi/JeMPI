a259d1835113e9b20154e4eeef3d567e3fc421ac
William Hayes <william.hayes@jembi.org>
2022-11-30 23:05:57 +0200

    Preliminary FHIR response

---
+++ JeMPI_Apps/JeMPI_Shared_Source/models/ExtendedLinkInfo.java
@@ -0,0 +1,8 @@
+package org.jembi.jempi.shared.models;
+
+import java.util.List;
+
+public record ExtendedLinkInfo(String stan,
+                               LinkInfo linkInfo,
+                               List<ExternalLinkCandidate> externalLinkCandidateList) {
+}
