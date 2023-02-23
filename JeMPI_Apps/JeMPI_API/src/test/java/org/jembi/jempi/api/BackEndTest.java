8b8b829f98f5af765c77c3e2789335f05271d427
Matthew Erispe <matthew.erispe@jembi.org>
2023-02-23 14:15:24 +0200

    update unit test reference class names

--- JeMPI_Apps/JeMPI_API/src/test/java/org/jembi/jempi/api/BackEndTest.java
+++ JeMPI_Apps/JeMPI_API/src/test/java/org/jembi/jempi/api/BackEndTest.java
@@ -1,157 +1,163 @@
 package org.jembi.jempi.api;
+
 import akka.actor.testkit.typed.javadsl.TestProbe;
 import akka.actor.typed.ActorRef;
 import akka.actor.testkit.typed.javadsl.ActorTestKit;
 import org.jembi.jempi.libmpi.LibMPI;
-import org.jembi.jempi.libmpi.MpiEntity;
-import org.jembi.jempi.libmpi.MpiExpandedGoldenRecord;
-import org.jembi.jempi.shared.models.CustomEntity;
-import org.jembi.jempi.shared.models.CustomGoldenRecord;
-import org.jembi.jempi.shared.models.SourceId;
+import org.jembi.jempi.shared.models.*;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
 import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
 class BackEndTest {
 
-    private static LibMPI libMPI;
+   private static LibMPI libMPI;
 
-    private static ActorTestKit testKit;
+   private static ActorTestKit testKit;
 
-    @BeforeAll
-    public static void setUp() {
-        testKit = ActorTestKit.create();
-        libMPI = mock(LibMPI.class);
-    }
+   @BeforeAll
+   public static void setUp() {
+      testKit = ActorTestKit.create();
+      libMPI = mock(LibMPI.class);
+   }
 
-    @AfterAll
-    public static void cleanup() {
-        testKit.shutdownTestKit();
-    }
+   @AfterAll
+   public static void cleanup() {
+      testKit.shutdownTestKit();
+   }
 
-    @Test
-    void testEventGetGoldenRecordDocumentsHandler() throws Exception {
-        CustomGoldenRecord customGoldenRecord1 = new CustomGoldenRecord(
-                "1234",
-                List.of(new SourceId("f1fa7b5c", "4e71", "11ec-8d3d-0242ac130003")),
-                "auxId",
-                "John",
-                "Doe",
-                "Male",
-                "1990-01-01",
-                "New York",
-                "555-1234",
-                "123-45-6789"
-        );
-        List<MpiEntity> mpiEntityList1 = List.of(
-                new MpiEntity(null, null),
-                new MpiEntity(null, null)
-        );
-        MpiExpandedGoldenRecord expandedGoldenRecord1 = new MpiExpandedGoldenRecord(customGoldenRecord1, mpiEntityList1);
+   @Test
+   void testEventGetGoldenRecordDocumentsHandler() throws Exception {
+      GoldenRecord goldenRecord1 = new GoldenRecord(
+            new PatientRecord(
+                  "1234",
+                  new SourceId("f1fa7b5c", "4e71", "11ec-8d3d-0242ac130003"),
+                  new CustomDemographicData(
+                        "auxId",
+                        "John",
+                        "Doe",
+                        "Male",
+                        "1990-01-01",
+                        "New York",
+                        "555-1234",
+                        "123-45-6789")
+            ));
 
-        CustomGoldenRecord customGoldenRecord2 = new CustomGoldenRecord(
-                "5678",
-                List.of(new SourceId("d0d38f0e", "4e71", "11ec-8d3d-0242ac130003")),
-                "auxId2",
-                "Jane",
-                "Smith",
-                "Female",
-                "1985-05-10",
-                "Los Angeles",
-                "555-5678",
-                "987-65-4321"
-        );
-        List<MpiEntity> mpiEntityList2 = List.of(
-                new MpiEntity(null, null),
-                new MpiEntity(null, null)
-        );
-        MpiExpandedGoldenRecord expandedGoldenRecord2 = new MpiExpandedGoldenRecord(customGoldenRecord2, mpiEntityList2);
+      List<PatientRecordWithScore> patientRecordWithScoreList1 = List.of(
+            new PatientRecordWithScore(null, null),
+            new PatientRecordWithScore(null, null));
 
-        CustomGoldenRecord customGoldenRecord3 = new CustomGoldenRecord(
-                "9012",
-                List.of(new SourceId("c44a67f6", "4e71", "11ec-8d3d-0242ac130003")),
-                "auxId3",
-                "Robert",
-                "Johnson",
-                "Male",
-                "1975-12-18",
-                "Chicago",
-                "555-9012",
-                "456-78-9012"
-        );
-        List<MpiEntity> mpiEntityList3 = List.of(
-                new MpiEntity( null, null),
-                new MpiEntity( null, null)
-        );
-        MpiExpandedGoldenRecord expandedGoldenRecord3 = new MpiExpandedGoldenRecord(customGoldenRecord3, mpiEntityList3);
-        List<MpiExpandedGoldenRecord> expandedGoldenRecords = List.of(expandedGoldenRecord1, expandedGoldenRecord2, expandedGoldenRecord3);
-        final var uids = List.of("1234", "5678", "9012");
+      ExpandedGoldenRecord expandedGoldenRecord1 = new ExpandedGoldenRecord(goldenRecord1, patientRecordWithScoreList1);
 
-        // mock LibMPI interface
-        final var libMPI = mock(LibMPI.class);
-        when(libMPI.getMpiExpandedGoldenRecordList(uids)).thenReturn(expandedGoldenRecords);
+      GoldenRecord goldenRecord2 = new GoldenRecord(
+            new PatientRecord(
+                  "5678",
+                  new SourceId("d0d38f0e", "4e71", "11ec-8d3d-0242ac130003"),
+                  new CustomDemographicData(
+                        "auxId2",
+                        "Jane",
+                        "Smith",
+                        "Female",
+                        "1985-05-10",
+                        "Los Angeles",
+                        "555-5678",
+                        "987-65-4321")
+            ));
 
-        // create ActorSystem and ActorRef using ActorTestKit
-        ActorTestKit testKit = ActorTestKit.create();
-        ActorRef<BackEnd.Event> myActorRef = testKit.spawn(BackEnd.create(libMPI));
+      List<PatientRecordWithScore> patientRecordWithScoreList2 = List.of(
+            new PatientRecordWithScore(null, null),
+            new PatientRecordWithScore(null, null));
 
-        // create TestProbe to receive response from actor
-        TestProbe<BackEnd.EventGetGoldenRecordDocumentsRsp> replyTo = testKit.createTestProbe();
+      ExpandedGoldenRecord expandedGoldenRecord2 = new ExpandedGoldenRecord(goldenRecord2, patientRecordWithScoreList2);
 
-        // send request to actor
-        myActorRef.tell(new BackEnd.EventGetGoldenRecordDocumentsReq(replyTo.getRef(), uids));
+      GoldenRecord goldenRecord3 = new GoldenRecord(
+            new PatientRecord(
+            "9012",
+            new SourceId("c44a67f6", "4e71", "11ec-8d3d-0242ac130003"),
+            new CustomDemographicData(
+                  "auxId3",
+               "Robert",
+               "Johnson",
+               "Male",
+               "1975-12-18",
+               "Chicago",
+               "555-9012",
+               "456-78-9012")
+            ));
 
-        // assert that response matches expected value
-        replyTo.expectMessage(new BackEnd.EventGetGoldenRecordDocumentsRsp(expandedGoldenRecords));
+      List<PatientRecordWithScore> patientRecordWithScoreList3 = List.of(
+            new PatientRecordWithScore(null, null),
+            new PatientRecordWithScore(null, null));
 
+      ExpandedGoldenRecord expandedGoldenRecord3 = new ExpandedGoldenRecord(goldenRecord3, patientRecordWithScoreList3);
 
-    }
+      List<ExpandedGoldenRecord> expandedGoldenRecords =
+            List.of(expandedGoldenRecord1, expandedGoldenRecord2, expandedGoldenRecord3);
+      final var uids = List.of("1234", "5678", "9012");
 
-    @Test
-    public void testFindPatientByUidEventHandler() {
-        // Create a mock libMPI instance
+      // mock LibMPI interface
+      final var libMPI = mock(LibMPI.class);
+      when(libMPI.getExpandedGoldenRecords(uids)).thenReturn(expandedGoldenRecords);
 
-        final var libMPI = mock(LibMPI.class);
+      // create ActorSystem and ActorRef using ActorTestKit
+      ActorTestKit testKit = ActorTestKit.create();
+      ActorRef<BackEnd.Event> myActorRef = testKit.spawn(BackEnd.create(libMPI));
 
-        // Create an instance of the actor to be tested
-        final var backend = testKit.spawn(BackEnd.create(libMPI));
+      // create TestProbe to receive response from actor
+      TestProbe<BackEnd.EventGetExpandedGoldenRecordsRsp> replyTo = testKit.createTestProbe();
 
-        // Create a TestProbe to receive responses
-        final var probe = testKit.createTestProbe(BackEnd.EventFindPatientRecordByUidResponse.class);
+      // send request to actor
+      myActorRef.tell(new BackEnd.EventGetExpandedGoldenRecordsReq(replyTo.getRef(), uids));
 
-        // Create an EventFindPatientByUidRequest message with a specific UID value
-        final var request = new BackEnd.EventFindPatientByUidRequest(probe.getRef(), "1234");
+      // assert that response matches expected value
+      replyTo.expectMessage(new BackEnd.EventGetExpandedGoldenRecordsRsp(expandedGoldenRecords));
+   }
 
-        // Stub the mock libMPI instance to return a CustomEntity object when `getDocument()` is called
-        final var customEntity = new CustomEntity(
-                "1234", // uid
-                new SourceId("f1fa7b5c", "4e71", "11ec-8d3d-0242ac130003"), // sourceId
-                "auxId", // auxId
-                "John", // givenName
-                "Doe", // familyName
-                "Male", // gender
-                "1990-01-01", // dob
-                "New York", // city
-                "555-1234", // phoneNumber
-                "123-45-6789" // nationalId
-        );
-        when(libMPI.getDocument("1234")).thenReturn(customEntity);
+   @Test
+   public void testFindPatientByUidEventHandler() {
+      // Create a mock libMPI instance
+      final var libMPI = mock(LibMPI.class);
 
-        // Send the message to the actor
-        backend.tell(request);
+      // Create an instance of the actor to be tested
+      final var backend = testKit.spawn(BackEnd.create(libMPI));
 
-        // Expect to receive the expected response
-        final var response = probe.receiveMessage();
-        assertEquals(customEntity, response.document());
+      // Create a TestProbe to receive responses
+      final var probe = testKit.createTestProbe(BackEnd.EventFindPatientRecordByUidResponse.class);
 
-        // Verify that libMPI was called with the correct arguments
-        verify(libMPI).startTransaction();
-        verify(libMPI).getDocument("1234");
-        verify(libMPI).closeTransaction();
-    }
+      // Create an EventFindPatientByUidRequest message with a specific UID value
+      final var request = new BackEnd.EventFindPatientByUidRequest(probe.getRef(), "1234");
 
+      // Stub the mock libMPI instance to return a CustomEntity object when `getDocument()` is called
+      final var patientRecord = new PatientRecord(
+            "1234", // uid
+            new SourceId("f1fa7b5c", "4e71", "11ec-8d3d-0242ac130003"), // sourceId
+            new CustomDemographicData(
+                  "auxId", // auxId
+               "John", // givenName
+               "Doe", // familyName
+               "Male", // gender
+               "1990-01-01", // dob
+               "New York", // city
+               "555-1234", // phoneNumber
+               "123-45-6789")); // nationalId
 
+      when(libMPI.getPatientRecord("1234")).thenReturn(patientRecord);
+
+      // Send the message to the actor
+      backend.tell(request);
+
+      // Expect to receive the expected response
+      final var response = probe.receiveMessage();
+      assertEquals(patientRecord, response.patient());
+
+      // Verify that libMPI was called with the correct arguments
+      verify(libMPI).startTransaction();
+      verify(libMPI).getPatientRecord("1234");
+      verify(libMPI).closeTransaction();
+   }
 }
