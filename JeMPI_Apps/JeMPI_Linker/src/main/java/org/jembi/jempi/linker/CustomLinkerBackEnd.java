41a08414026fe3e272cf7dfb7bf129072c174ef1
Matthew Erispe <matthew.erispe@jembi.org>
2023-03-16 09:11:41 +0200

    JMPI-174 include new method in linker backend scala

--- JeMPI_Apps/JeMPI_Linker/src/main/java/org/jembi/jempi/linker/CustomLinkerBackEnd.java
+++ JeMPI_Apps/JeMPI_Linker/src/main/java/org/jembi/jempi/linker/CustomLinkerBackEnd.java
@@ -16,27 +16,22 @@ public final class CustomLinkerBackEnd {
       final var expandedGoldenRecord = libMPI.findExpandedGoldenRecords(List.of(goldenId)).get(0);
       final var goldenRecord = expandedGoldenRecord.goldenRecord();
       final var demographicData = goldenRecord.demographicData();
-      try {
-         BackEnd.updateGoldenRecordField(expandedGoldenRecord,
-                 "givenName", demographicData.givenName(), CustomDemographicData::givenName);
-         BackEnd.updateGoldenRecordField(expandedGoldenRecord,
-                 "familyName", demographicData.familyName(), CustomDemographicData::familyName);
-         BackEnd.updateGoldenRecordField(expandedGoldenRecord,
-                 "gender", demographicData.gender(), CustomDemographicData::gender);
-         BackEnd.updateGoldenRecordField(expandedGoldenRecord,
-                 "dob", demographicData.dob(), CustomDemographicData::dob);
-         BackEnd.updateGoldenRecordField(expandedGoldenRecord,
-                 "city", demographicData.city(), CustomDemographicData::city);
-         BackEnd.updateGoldenRecordField(expandedGoldenRecord,
-                 "phoneNumber", demographicData.phoneNumber(), CustomDemographicData::phoneNumber);
-         BackEnd.updateGoldenRecordField(expandedGoldenRecord,
-                 "nationalId", demographicData.nationalId(), CustomDemographicData::nationalId);
 
-         BackEnd.updateMatchingPatientRecordScoreForGoldenRecord(expandedGoldenRecord, goldenId);
-      } catch (Exception e) {
-         throw new RuntimeException(e);
-      }
-
+      BackEnd.updateGoldenRecordField(expandedGoldenRecord,
+                                      "givenName", demographicData.givenName(), CustomDemographicData::givenName);
+      BackEnd.updateGoldenRecordField(expandedGoldenRecord,
+                                      "familyName", demographicData.familyName(), CustomDemographicData::familyName);
+      BackEnd.updateGoldenRecordField(expandedGoldenRecord,
+                                      "gender", demographicData.gender(), CustomDemographicData::gender);
+      BackEnd.updateGoldenRecordField(expandedGoldenRecord,
+                                      "dob", demographicData.dob(), CustomDemographicData::dob);
+      BackEnd.updateGoldenRecordField(expandedGoldenRecord,
+                                      "city", demographicData.city(), CustomDemographicData::city);
+      BackEnd.updateGoldenRecordField(expandedGoldenRecord,
+                                      "phoneNumber", demographicData.phoneNumber(), CustomDemographicData::phoneNumber);
+      BackEnd.updateGoldenRecordField(expandedGoldenRecord,
+                                      "nationalId", demographicData.nationalId(), CustomDemographicData::nationalId);
+      BackEnd.updateMatchingPatientRecordScoreForGoldenRecord(expandedGoldenRecord, goldenId);
 
    }
 
