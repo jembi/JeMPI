c98a5f476ba5e318f4556aa1e1e7dd15b276d265
Matthew Erispe <matthew.erispe@jembi.org>
2023-07-20 07:16:34 +0200

    add enable preview setting to java scripts

--- native/helper/java/start-app-async-receiver.sh
+++ native/helper/java/start-app-async-receiver.sh
@@ -3,4 +3,4 @@
 set -e
 set -u
 
-java -DLOG4J2_LEVEL="DEBUG" -DKAFKA_BOOTSTRAP_SERVERS="localhost:29092" -DKAFKA_CLIENT_ID="client-id-syncrx" -DCSV_DIR="./data-async-receiver/csv/" -jar ../JeMPI_Apps/JeMPI_AsyncReceiver/target/AsyncReceiver-1.0-SNAPSHOT-spring-boot.jar
+java --enable-preview -DLOG4J2_LEVEL="DEBUG" -DKAFKA_BOOTSTRAP_SERVERS="localhost:29092" -DKAFKA_CLIENT_ID="client-id-syncrx" -DCSV_DIR="./data-async-receiver/csv/" -jar ../JeMPI_Apps/JeMPI_AsyncReceiver/target/AsyncReceiver-1.0-SNAPSHOT-spring-boot.jar
--- native/helper/java/start-app-etl.sh
+++ native/helper/java/start-app-etl.sh
@@ -3,4 +3,4 @@
 set -e
 set -u
 
-java -DLOG4J2_LEVEL="DEBUG" -DKAFKA_BOOTSTRAP_SERVERS="localhost:29092" -DKAFKA_APPLICATION_ID="client-id-etl" -jar ../JeMPI_Apps/JeMPI_ETL/target/ETL-1.0-SNAPSHOT-spring-boot.jar
+java --enable-preview -DLOG4J2_LEVEL="DEBUG" -DKAFKA_BOOTSTRAP_SERVERS="localhost:29092" -DKAFKA_APPLICATION_ID="app-id-etl" -jar ../JeMPI_Apps/JeMPI_ETL/target/ETL-1.0-SNAPSHOT-spring-boot.jar
