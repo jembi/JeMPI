0cf8c4841658447919db661666a49e5ca12ba560
William Hayes <william.hayes@jembi.org>
2023-07-24 09:31:57 +0200

    tidy up log

--- JeMPI_Apps/JeMPI_LibMPI/src/main/java/org/jembi/jempi/libmpi/dgraph/DgraphMutations.java
+++ JeMPI_Apps/JeMPI_LibMPI/src/main/java/org/jembi/jempi/libmpi/dgraph/DgraphMutations.java
@@ -34,7 +34,6 @@ final class DgraphMutations {
          LOGGER.error("Failed to insert interaction");
          return null;
       }
-      LOGGER.debug("{}", result);
       final var grUID = cloneGoldenRecordFromInteraction(interaction.demographicData(),
                                                          result.interactionUID,
                                                          result.sourceUID,
@@ -43,7 +42,6 @@ final class DgraphMutations {
                                                                                           true,
                                                                                           interaction.uniqueInteractionData()
                                                                                                      .auxId()));
-      LOGGER.debug("grUID: {}", grUID);
       if (grUID == null) {
          LOGGER.error("Failed to insert golden record");
          return null;
