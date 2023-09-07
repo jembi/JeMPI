2e00cfd36aedc4cb6deeabef30a649f0bd9250b7
Ryan Crichton <ryan@jembi.org>
2023-09-07 13:20:56 +0200

    Add build script comment

--- JeMPI_Apps/JeMPI_UI/build-image.sh
+++ JeMPI_Apps/JeMPI_UI/build-image.sh
@@ -12,4 +12,5 @@ envsubst < ../../docker/conf/ui/.env > ./.env
 
 [ -z $(docker images -q ${UI_IMAGE}) ] || docker rmi ${UI_IMAGE}
 docker system prune --volumes -f
+# Injects env vars in .env as build args for use when the UI is built
 docker build --tag "$UI_IMAGE" $(cat .env | sed '/^ *$/d' | sed 's@^@--build-arg @g' | paste -s -d " ") --target production-stage .
