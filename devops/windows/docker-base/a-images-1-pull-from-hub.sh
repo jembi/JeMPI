6b738b355cb08a070dbd3c7a02356c36f0d1fcc3
William Hayes <william.hayes@jembi.org>
2023-09-23 14:42:40 +0200

    jempi-base-windows

--- devops/windows/docker-base/a-images-1-pull-from-hub.sh
+++ devops/windows/docker-base/a-images-1-pull-from-hub.sh
@@ -3,11 +3,9 @@
 set -e
 set -u
 
-source ./0-conf.env
+source ./conf.env
 source ./conf/images/conf-hub-images.sh
-source ./conf/images/conf-app-images.sh
 
-docker pull $JAVA_BASE_IMAGE
 docker pull $REGISTRY_IMAGE
 #docker pull $KEYCLOAK_IMAGE
 docker pull $HAPROXY_IMAGE
