9a0c2a62d6f84eb1ac54ad291db6e55ca4f04b93
w-hayes <william.hayes@jembi.org>
2023-09-23 18:16:50 +0200

    [partial jempi-base-windows

--- devops/windows/windows-apps/start.ps1
+++ devops/windows/windows-apps/start.ps1
@@ -1,36 +1,45 @@
+$kafka1IP = '192.168.88.252'
+$JeMPI_Apps = "..\..\..\..\..\JeMPI_Apps"
+$kafka_bootstrap_servers = "-DKAFKA_BOOTSTRAP_SERVERS=" + $kafka1IP + ":9094"
+
+$async_receiver_folder = '.\app_data\async_receiver'
+
+$async_receiver_jar = "-jar " + $JeMPI_Apps + "\JeMPI_AsyncReceiver\target\AsyncReceiver-1.0-SNAPSHOT-spring-boot.jar"
+$async_reveiver_log4j_level = "-DLOG4J2_LEVEL=`"DEBUG`""
+$async_receiver_kafka_client_id = "-DKAFKA_CLIENT_ID=client-id-syncrx"
+
+
+
+$scriptpath = $MyInvocation.MyCommand.Path
+$dir = Split-Path $scriptpath
+Set-Location $dir
 
 # 
 # build apps
 #
-
-#cd Z:\devops\windows\windows-apps
-$scriptpath = $MyInvocation.MyCommand.Path
-$dir = Split-Path $scriptpath
-cd $dir
-
-pushd ..\..\..\JeMPI_Apps
+Push-Location ..\..\..\JeMPI_Apps
   mvn clean  
   mvn package
-popd
-$AsyncReceiverFolder = '.\app_data\async_receiver'
+Pop-Location
+
 
 #
 # start async receiver
 #
-if (Test-path $AsyncReceiverFolder\app\csv) {
-  Write-Host "$AsyncReceiverFolder\app\csv Folder Exists"
-} else { 
-  New-Item $AsyncReceiverFolder\app\csv -ItemType Directory
+if (Test-path $async_receiver_folder\csv) {
+  Write-Host ${async_receiver_folder}'\csv exists'   
+} else {
+  New-Item $async_receiver_folder\csv -ItemType Directory
   Write-Host 'Folder Created successfully'
 }
-
-Start-Process -FilePath java `
-              -ArgumentList "-DLOG4J2_LEVEL=`"DEBUG`"", `
-                            "-DKAFKA_BOOTSTRAP_SERVERS=192.168.88.252:9094", `
-                            "-DKAFKA_CLIENT_ID=client-id-syncrx", `
-                            "-jar ..\..\..\..\..\..\JeMPI_Apps\JeMPI_AsyncReceiver\target\AsyncReceiver-1.0-SNAPSHOT-spring-boot.jar" `
-              -WindowStyle Normal `
-              -WorkingDirectory .\$AsyncReceiverFolder\app `
-              -Debug `
-              -Verbose
-
+$async_handle = Start-Process -FilePath java `
+                              -ArgumentList $async_reveiver_log4j_level, `
+                                            $kafka_bootstrap_servers, `
+                                            $async_receiver_kafka_client_id, `
+                                            $async_receiver_jar `
+                              -WindowStyle Normal `
+                              -WorkingDirectory $async_receiver_folder `
+                              -Debug `
+                              -Verbose `
+                              -PassThru
+$async_handle | Export-Clixml -Path (Join-Path './' 'async_handle.xml')
---
+++ devops/windows/windows-apps/stop.ps1
@@ -0,0 +1,2 @@
+﻿$async_handle = Import-Clixml -Path (Join-Path './' 'async_handle.xml')
+$async_handle | Stop-Process
