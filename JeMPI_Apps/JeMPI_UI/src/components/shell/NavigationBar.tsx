8d42f2f852ba027174c17271f6958169afc392ab
hexa <issam.baccouch@proton.me>
2023-09-25 17:28:53 +0100

    refactor: top bar and enhance pwa

--- JeMPI_Apps/JeMPI_UI/src/components/shell/NavigationBar.tsx
+++ JeMPI_Apps/JeMPI_UI/src/components/shell/NavigationBar.tsx
@@ -1,57 +1,174 @@
+import React, { useState } from 'react'
+import {
+  AppBar,
+  Box,
+  Divider,
+  Drawer,
+  IconButton,
+  Toolbar
+} from '@mui/material'
 import {
   Dashboard as DashboardIcon,
   People as PeopleIcon,
   Search as SearchIcon
 } from '@mui/icons-material'
+import MenuIcon from '@mui/icons-material/Menu'
 import UploadFileIcon from '@mui/icons-material/UploadFile'
-import { AppBar, Box, Toolbar } from '@mui/material'
-import Button from '../shared/Button'
 import JembiLogo from './JembiLogo'
 import NavigationMenu from './NavigationMenu'
+import { Link } from '@tanstack/react-location'
 
-const colorGradient =
-  'linear-gradient(90.05deg, #8BF280 -4.51%, #3B826B -4.5%, #58AB73 99.95%)'
+const LabeledIconBox = ({
+  icon,
+  label,
+  link
+}: {
+  icon: JSX.Element
+  label: string
+  link: string
+}) => (
+  <Link to={`${link}`} className="unset-decoration">
+    <Box
+      sx={{
+        display: 'grid',
+        gridTemplateColumns: 'repeat(2, auto)',
+        fontWeight: '500',
+        justifyContent: 'start',
+        width: '100%',
+        gap: 1
+      }}
+    >
+      {icon}
+      {label}
+    </Box>
+  </Link>
+)
+
+const drawerWidth = 240
+
+const navigationItems = [
+  { icon: <DashboardIcon />, label: 'Dashboard', link: '/' },
+  { icon: <SearchIcon />, label: 'Browse', link: '/browse-records' },
+  { icon: <PeopleIcon />, label: 'Notifications', link: '/notifications' },
+  { icon: <UploadFileIcon />, label: 'Import', link: '/import' }
+]
 
 const NavigationBar = () => {
-  return (
-    <AppBar position="fixed">
-      <Toolbar>
+  const container = window?.document?.body
+  const [mobileOpen, setMobileOpen] = useState(false)
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(prevState => !prevState)
+  }
+  const drawer = (
+    <>
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center'
+        }}
+      >
         <JembiLogo />
-        <Box sx={{ flexGrow: 1 }}>
-          <Button href="/" startIcon={<DashboardIcon />} className="Appbar">
-            Dashboard
-          </Button>
-          <Button
-            href="/browse-records"
-            startIcon={<SearchIcon />}
-            className="Appbar"
-          >
-            Browse
-          </Button>
-          <Button
-            href="/notifications"
-            startIcon={<PeopleIcon />}
-            className="Appbar"
-          >
-            Notifications
-          </Button>
-          <Button
-            href="/import"
-            startIcon={<UploadFileIcon />}
-            className="Appbar"
-          >
-            IMPORT
-          </Button>
-        </Box>
-        <NavigationMenu />
-      </Toolbar>
+      </div>
+      <Divider />
       <Box
         sx={{
-          background: '#274263',
-          height: 12
+          display: 'grid',
+          gridTemplateColumns: 'repeat(1, auto)',
+          fontWeight: '500',
+          justifyContent: 'start',
+          alignItems: 'end',
+          gap: 3,
+          padding: '2rem 1rem 1rem 1rem'
         }}
-      />
-    </AppBar>
+      >
+        {navigationItems.map(item => (
+          <LabeledIconBox
+            key={item.label}
+            icon={item.icon}
+            label={item.label}
+            link={item.link}
+          />
+        ))}
+      </Box>
+      <NavigationMenu />
+    </>
+  )
+
+  return (
+    <>
+      <nav>
+        <Drawer
+          container={container}
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true // Better open performance on mobile.
+          }}
+          sx={{
+            display: { xs: 'block', md: 'none' },
+            '& .MuiDrawer-paper': {
+              boxSizing: 'border-box',
+              width: drawerWidth
+            }
+          }}
+        >
+          {drawer}
+        </Drawer>
+      </nav>
+      <AppBar position="fixed">
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{
+              mr: 2,
+              display: { xs: 'block', md: 'none' },
+              color: '#000'
+            }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Box
+            sx={{
+              display: { xs: 'grid', md: 'block' },
+              justifyContent: { xs: 'center', md: 'start' },
+              width: { xs: '100%', md: 'auto' }
+            }}
+          >
+            <JembiLogo />
+          </Box>
+          <Box
+            sx={{
+              display: { xs: 'none', md: 'grid' },
+              gridTemplateColumns: 'repeat(4, auto)',
+              fontWeight: '500',
+              gap: 3
+            }}
+          >
+            {navigationItems.map(item => (
+              <LabeledIconBox
+                key={item.label}
+                icon={item.icon}
+                label={item.label}
+                link={item.link}
+              />
+            ))}
+          </Box>
+          <NavigationMenu />
+        </Toolbar>
+        <Box
+          sx={{
+            background: '#274263',
+            height: 3
+          }}
+        />
+      </AppBar>
+    </>
   )
 }
 
