3c39ad0026de9970e256f7b9d66d04c75427a900
Issam Baccouch <issam.baccouch@proton.me>
2023-10-04 15:14:30 +0100

    feat: add tailwind utility-first css

---
+++ JeMPI_Apps/JeMPI_UI/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  important: true,
+  corePlugins: {
+    preflight: false
+  },
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {}
+  },
+  plugins: []
+}
