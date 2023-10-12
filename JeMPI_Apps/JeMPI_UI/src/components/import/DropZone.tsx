28a17dc95d0f85d2300b4152abb2c5b8a3c7e0a4
Issam Baccouch <issam.baccouch@proton.me>
2023-10-12 18:14:15 +0100

    chore: enhance handle size formatting

--- JeMPI_Apps/JeMPI_UI/src/components/import/DropZone.tsx
+++ JeMPI_Apps/JeMPI_UI/src/components/import/DropZone.tsx
@@ -21,10 +21,11 @@ import { FileObj, UploadStatus } from '../../types/FileUpload'
 import Button from '../shared/Button'
 import './Import.css'
 import UploadFileListItem from './UploadFileListItem'
-import { megabytesToBytes } from 'utils/formatters'
+import { formatBytesSize, megabytesToBytes } from 'utils/formatters'
 
-const MAX_UPLOAD_FILE_SIZE_IN_MEGABYTES =
-  process.env.REACT_APP_MAX_UPLOAD_CSV_SIZE_IN_MEGABYTES ?? '128'
+const MAX_UPLOAD_FILE_SIZE_IN_BYTES = megabytesToBytes(
+  +(process.env.REACT_APP_MAX_UPLOAD_CSV_SIZE_IN_MEGABYTES ?? 128)
+)
 const DropZone: FC = () => {
   const [fileObjs, setFilesObj] = useState<FileObj | undefined>()
   const abortControllerRef = useRef<AbortController>(new AbortController())
@@ -65,7 +66,7 @@ const DropZone: FC = () => {
     accept: { 'text/csv': ['.csv'] },
     onDrop,
     multiple: false,
-    maxSize: megabytesToBytes(+MAX_UPLOAD_FILE_SIZE_IN_MEGABYTES)
+    maxSize: MAX_UPLOAD_FILE_SIZE_IN_BYTES
   })
 
   const uploadFile = async (fileObj: FileObj) => {
@@ -193,7 +194,7 @@ const DropZone: FC = () => {
             <a>Click to upload</a> or drag and drop
           </Typography>
           <Typography color="#00000099" fontSize="1rem">
-            CSV (max. {MAX_UPLOAD_FILE_SIZE_IN_MEGABYTES} MB)
+            CSV (max. {formatBytesSize(MAX_UPLOAD_FILE_SIZE_IN_BYTES)})
           </Typography>
         </div>
       </Box>
