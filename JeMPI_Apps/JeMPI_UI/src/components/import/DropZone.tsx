c4045c6adc6a63302abf0cf121d1a7bebcf60069
Issam Baccouch <issam.baccouch@proton.me>
2023-10-16 15:11:36 +0100

    feat: add api queries and form handler

--- JeMPI_Apps/JeMPI_UI/src/components/import/DropZone.tsx
+++ JeMPI_Apps/JeMPI_UI/src/components/import/DropZone.tsx
@@ -6,7 +6,6 @@ import {
   CardActions,
   CardContent,
   Checkbox,
-  FormControlLabel,
   Grid,
   TextField,
   Typography
@@ -17,24 +16,50 @@ import { useSnackbar } from 'notistack'
 import { FC, useRef, useState } from 'react'
 import { FileRejection, useDropzone } from 'react-dropzone'
 import ApiClient from '../../services/ApiClient'
-import { FileObj, UploadStatus } from '../../types/FileUpload'
+import {
+  FileObj,
+  UploadStatus,
+  importQueriesType
+} from '../../types/FileUpload'
 import Button from '../shared/Button'
 import UploadFileListItem from './UploadFileListItem'
 import { formatBytesSize, megabytesToBytes } from 'utils/formatters'
+import { useFormik } from 'formik'
 
 const MAX_UPLOAD_FILE_SIZE_IN_BYTES = megabytesToBytes(
   +(process.env.REACT_APP_MAX_UPLOAD_CSV_SIZE_IN_MEGABYTES ?? 128)
 )
+
 const DropZone: FC = () => {
+  const {
+    handleChange,
+    handleSubmit,
+    values: FormValues
+  } = useFormik({
+    initialValues: {
+      reporting: false,
+      computing: false,
+      leftMargin: 0,
+      rightMargin: 0,
+      threshold: 0,
+      windowSize: 0
+    },
+    onSubmit: () => {
+      if (fileObjs) {
+        uploadFileMutation.mutate(fileObjs)
+      }
+    }
+  })
+
+  const { enqueueSnackbar } = useSnackbar()
   const [fileObjs, setFilesObj] = useState<FileObj | undefined>()
   const abortControllerRef = useRef<AbortController>(new AbortController())
-  const { enqueueSnackbar } = useSnackbar()
 
   const onDrop = (
     acceptedFiles: File[],
     fileRejections: FileRejection[]
   ): void => {
-    validate(acceptedFiles, fileRejections)
+    validate(fileRejections)
     setFilesObj({
       file: acceptedFiles[0],
       progress: 0,
@@ -42,20 +67,10 @@ const DropZone: FC = () => {
     })
   }
 
-  const validate = (
-    acceptedFiles: File[],
-    fileRejections: FileRejection[]
-  ): void => {
+  const validate = (fileRejections: FileRejection[]): void => {
     if (fileRejections.length > 0) {
       enqueueSnackbar(fileRejections[0].errors[0].message, {
         variant: 'error'
-      })
-      return
-    }
-
-    if (uploadFileMutation.isLoading) {
-      enqueueSnackbar('Please wait for current import to be completed', {
-        variant: 'warning'
       })
       return
     }
@@ -68,15 +83,22 @@ const DropZone: FC = () => {
     maxSize: MAX_UPLOAD_FILE_SIZE_IN_BYTES
   })
 
-  const uploadFile = async (fileObj: FileObj) => {
-    return await ApiClient.uploadFile(createFileUploadAxiosConfig(fileObj))
+  const uploadFile = async (
+    fileObj: FileObj,
+    importQueries: importQueriesType
+  ) => {
+    return await ApiClient.uploadFile(
+      createFileUploadAxiosConfig(fileObj, importQueries)
+    )
   }
 
   const createFileUploadAxiosConfig = (
-    fileObj: FileObj
+    fileObj: FileObj,
+    importQueries: importQueriesType
   ): AxiosRequestConfig<FormData> => {
     const formData = new FormData()
     formData.set('csv', fileObj.file)
+    formData.set('queries', JSON.stringify(importQueries))
     return {
       signal: abortControllerRef.current.signal,
       headers: {
@@ -100,7 +122,7 @@ const DropZone: FC = () => {
   }
 
   const uploadFileMutation = useMutation({
-    mutationFn: uploadFile,
+    mutationFn: (fileObjs: FileObj) => uploadFile(fileObjs, FormValues),
     onSuccess: (_, fileObj) => {
       setFilesObj((prev: FileObj | undefined) =>
         prev ? { ...prev, status: UploadStatus.Complete } : undefined
@@ -128,16 +150,6 @@ const DropZone: FC = () => {
     abortControllerRef.current = new AbortController()
     uploadFileMutation.reset()
     setFilesObj(undefined)
-  }
-
-  const handleUpload = () => {
-    if (
-      fileObjs?.status === UploadStatus.Complete ||
-      fileObjs?.status === UploadStatus.Failed
-    ) {
-      return
-    }
-    if (fileObjs) uploadFileMutation.mutate(fileObjs)
   }
 
   const handleRemoveFile = (): void => {
@@ -188,95 +200,108 @@ const DropZone: FC = () => {
           ) : (
             uploadList
           )}
-          <Grid
-            container
-            spacing={2}
-            sx={{ marginTop: '1rem' }}
-            alignItems="center"
-          >
-            <Grid item xs={9}>
-              <Typography fontWeight="bold" fontSize="1rem">
-                Generate Report:
-              </Typography>
+          <form>
+            <Grid
+              container
+              spacing={2}
+              sx={{ marginTop: '1rem' }}
+              alignItems="center"
+            >
+              <Grid item xs={9}>
+                <Typography fontWeight="bold" fontSize="1rem">
+                  Generate Report:
+                </Typography>
+              </Grid>
+              <Grid item xs={3}>
+                <Checkbox
+                  name="reporting"
+                  checked={FormValues.reporting}
+                  value={FormValues.reporting}
+                  onChange={handleChange}
+                />
+              </Grid>
+              <Grid item xs={9}>
+                <Typography fontWeight="bold" fontSize="1rem">
+                  Compute M&U before linking:
+                </Typography>
+              </Grid>
+              <Grid item xs={3}>
+                <Checkbox
+                  name="computing"
+                  checked={FormValues.computing}
+                  value={FormValues.computing}
+                  onChange={handleChange}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <Typography fontWeight="bold" fontSize="1rem">
+                  Linking:
+                </Typography>
+              </Grid>
+              <Grid item xs={12} md={4}>
+                <TextField
+                  name="leftMargin"
+                  type="number"
+                  size="small"
+                  variant="outlined"
+                  label="Notification Left margin"
+                  value={FormValues.leftMargin}
+                  onChange={handleChange}
+                  inputProps={{ min: 0, max: 100, step: 0.01 }}
+                  fullWidth
+                />
+              </Grid>
+              <Grid item xs={12} md={4}>
+                <TextField
+                  name="threshold"
+                  type="number"
+                  size="small"
+                  variant="outlined"
+                  label="Threshold"
+                  value={FormValues.threshold}
+                  onChange={handleChange}
+                  inputProps={{ min: 0, max: 100, step: 0.01 }}
+                  fullWidth
+                />
+              </Grid>
+              <Grid item xs={12} md={4}>
+                <TextField
+                  name="rightMargin"
+                  type="number"
+                  size="small"
+                  variant="outlined"
+                  label="Notification right margin"
+                  value={FormValues.rightMargin}
+                  onChange={handleChange}
+                  inputProps={{ min: 0, max: 100, step: 0.01 }}
+                  fullWidth
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <Typography fontWeight="bold" fontSize="1rem">
+                  Notification for similar Candidates:
+                </Typography>
+              </Grid>
+              <Grid item xs={12} md={4}>
+                <TextField
+                  name="windowSize"
+                  type="number"
+                  size="small"
+                  variant="outlined"
+                  label="Window size"
+                  value={FormValues.windowSize}
+                  onChange={handleChange}
+                  inputProps={{ min: 0, max: 100, step: 0.01 }}
+                  fullWidth
+                />
+              </Grid>
             </Grid>
-            <Grid item xs={3}>
-              <FormControlLabel
-                value="false"
-                control={<Checkbox />}
-                label=""
-                labelPlacement="start"
-              />
-            </Grid>
-            <Grid item xs={9}>
-              <Typography fontWeight="bold" fontSize="1rem">
-                Compute M&U before linking:
-              </Typography>
-            </Grid>
-            <Grid item xs={3}>
-              <FormControlLabel
-                value="true"
-                control={<Checkbox />}
-                label=""
-                labelPlacement="start"
-                checked={true}
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <Typography fontWeight="bold" fontSize="1rem">
-                Linking:
-              </Typography>
-            </Grid>
-            <Grid item xs={12} md={4}>
-              <TextField
-                type="number"
-                size="small"
-                variant="outlined"
-                label="Notification Left margin"
-                inputProps={{ min: 0, max: 100, step: 0.01 }}
-                fullWidth
-              />
-            </Grid>
-            <Grid item xs={12} md={4}>
-              <TextField
-                type="number"
-                size="small"
-                variant="outlined"
-                label="Threshold"
-                inputProps={{ min: 0, max: 100, step: 0.01 }}
-                fullWidth
-              />
-            </Grid>
-            <Grid item xs={12} md={4}>
-              <TextField
-                type="number"
-                size="small"
-                variant="outlined"
-                label="Notification right margin"
-                inputProps={{ min: 0, max: 100, step: 0.01 }}
-                fullWidth
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <Typography fontWeight="bold" fontSize="1rem">
-                Notification for similar Candidates:
-              </Typography>
-            </Grid>
-            <Grid item xs={12} md={4}>
-              <TextField
-                type="number"
-                size="small"
-                variant="outlined"
-                label="Window size"
-                inputProps={{ min: 0, max: 100, step: 0.01 }}
-                fullWidth
-              />
-            </Grid>
-          </Grid>
+          </form>
         </CardContent>
         <CardActions sx={{ display: 'block', textAlign: 'center' }}>
           <Button
             variant="contained"
-            onClick={handleUpload}
+            onClick={() => handleSubmit()}
             disabled={
               uploadFileMutation.isLoading ||
               uploadFileMutation.isError ||
