c6613a085ef2726f4e1c25b606572c35589589b9
ChidoW <warambwac@gmail.com>
2023-11-14 02:08:29 +0200

    Updating global topic logic

--- JeMPI_Apps/JeMPI_LibShared/src/main/java/org/jembi/jempi/shared/kafka/globalContext/globalKTableWrapper/serde/KTableSerde.java
+++ JeMPI_Apps/JeMPI_LibShared/src/main/java/org/jembi/jempi/shared/kafka/globalContext/globalKTableWrapper/serde/KTableSerde.java
@@ -3,7 +3,7 @@ package org.jembi.jempi.shared.kafka.globalContext.globalKTableWrapper.serde;
 import org.apache.kafka.common.serialization.Serdes;
 
 public class KTableSerde<T> extends Serdes.WrapperSerde<T> {
-    public KTableSerde() {
-        super(new KTableSerializer<>(), new KTableDeserializer<>());
+    public KTableSerde(Class<T> serializeCls) {
+        super(new KTableSerializer<T>(), new KTableDeserializer<T>(serializeCls));
     }
 }
--- JeMPI_Apps/JeMPI_LibShared/src/test/java/org/jembi/jempi/shared/kafka/globalContext/GlobalKTableWrapperTest.java
+++ JeMPI_Apps/JeMPI_LibShared/src/test/java/org/jembi/jempi/shared/kafka/globalContext/GlobalKTableWrapperTest.java
@@ -1,42 +1,65 @@
 package org.jembi.jempi.shared.kafka.globalContext;
 
+import org.apache.kafka.clients.admin.DeleteTopicsOptions;
+import org.apache.kafka.clients.admin.ListTopicsOptions;
+import org.apache.kafka.clients.admin.TopicListing;
+import org.apache.kafka.streams.errors.StreamsException;
 import org.jembi.jempi.shared.kafka.globalContext.globalKTableWrapper.GlobalKTableWrapper;
 import org.jembi.jempi.shared.kafka.globalContext.globalKTableWrapper.GlobalKTableWrapperInstance;
 
+import java.util.Collection;
+import java.util.ArrayList;
 import java.util.concurrent.ExecutionException;
+import java.util.stream.Collectors;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class GlobalKTableWrapperTest {
 
-    class MockTableData{
-        private int count;
+    TestUtils testUtils;
 
-        public int getCount() {
-            return count;
+    @BeforeAll
+    void prepareForTests(){
+        testUtils = new TestUtils("localhost:9092");
+    }
+
+    GlobalKTableWrapper getGlobalKTableWrapperInstance(Boolean restAll) throws ExecutionException, InterruptedException {
+        if (restAll){
+            Collection<String> collection = testUtils.kafkaAdminClient.listTopics(new ListTopicsOptions().listInternal(false)).listings().get().stream()
+                    .map(TopicListing::name)
+                    .filter(name -> name.startsWith("testTopic"))
+                    .collect(Collectors.toCollection(ArrayList::new));
+
+
+            testUtils.kafkaAdminClient.deleteTopics(collection, new DeleteTopicsOptions()).all().get();
+            Thread.sleep(1000);
         }
+        return new GlobalKTableWrapper(testUtils.bootStrapServer);
     }
-
-    String getTestTopicName(final String topicName){
-        return String.format("testTopic-%s", topicName);
-    }
-    GlobalKTableWrapper getGlobalKTableWrapperInstance(Boolean restAll){
-        return new GlobalKTableWrapper("localhost:9092");
-    }
-
     @Test
     void testCanCreateNewInstance() throws ExecutionException, InterruptedException {
-        GlobalKTableWrapperInstance<MockTableData> sampleInstance =  getGlobalKTableWrapperInstance(true).getCreate(getTestTopicName("sample-table"));
+        GlobalKTableWrapperInstance<TestUtils.MockTableData> sampleInstance = getGlobalKTableWrapperInstance(true).getCreate(testUtils.getTestTopicName("sample-table"), TestUtils.MockTableData.class);
+        assertInstanceOf(GlobalKTableWrapperInstance.class, sampleInstance);
     }
-
-    void testItErrorsOutWhenGlobalKTableDoesNotExists(){
-
+    @Test
+    void testItErrorsOutWhenGlobalKTableDoesNotExists() throws ExecutionException, InterruptedException {
+        assertThrows(StreamsException.class, () -> {
+            getGlobalKTableWrapperInstance(true).get(testUtils.getTestTopicName("sample-table"), TestUtils.MockTableData.class);
+        });
     }
+    @Test
+    void testDoesNotRecreateIfGlobalKTableAlreadyExists() throws ExecutionException, InterruptedException {
+        GlobalKTableWrapper gktableWrapper = getGlobalKTableWrapperInstance(true);
+        GlobalKTableWrapperInstance<TestUtils.MockTableData> sampleInstance = gktableWrapper.getCreate(testUtils.getTestTopicName("sample-table"), TestUtils.MockTableData.class);
+        assertInstanceOf(GlobalKTableWrapperInstance.class, sampleInstance);
 
-    void testDoesNotRecreateIfGlobalKTableAlreadyExists(){
+        assertEquals(sampleInstance.hashCode(), gktableWrapper.get(testUtils.getTestTopicName("sample-table"), TestUtils.MockTableData.class).hashCode());
+
 
     }
 }
