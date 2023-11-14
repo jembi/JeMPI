1eb602c2598998f893b88e90512de7a41c3fc3a5
William Hayes <william.hayes@jembi.org>
2023-11-14 14:05:21 +0200

    cleaned up scripts

---
+++ devops/linux/docker/d-stack-1-build-ui-reboot.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -e
+set -u
+
+echo
+echo "Down stacks"
+source ./helper/scripts/d-stack-rm.sh
+
+echo
+echo "Build Apps"
+pwd
+pushd ../../../JeMPI_Apps
+  source ./build-all-ui.sh
+popd
+sleep 2
+
+source ./helper/scripts/d-stack-wait-removed.sh
+echo
+echo Up app containers
+source ./helper/scripts/d-stack-create-dirs.sh
+sleep 2
+source ./helper/scripts/d-stack-deploy-0.sh
+sleep 2
+source ./helper/scripts/d-stack-up-hub-containers.sh
+sleep 2
+source ./helper/scripts/d-stack-up-app-containers.sh
