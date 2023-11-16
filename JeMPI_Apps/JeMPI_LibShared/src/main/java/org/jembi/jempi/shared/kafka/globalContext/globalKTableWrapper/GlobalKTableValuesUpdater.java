2861e3a0c2515335462de77ef960c52afe17e6e5
ChidoW <warambwac@gmail.com>
2023-11-16 09:13:05 +0200

    Updating Kafka global tables

---
+++ JeMPI_Apps/JeMPI_LibShared/src/main/java/org/jembi/jempi/shared/kafka/globalContext/globalKTableWrapper/GlobalKTableValuesUpdater.java
@@ -0,0 +1,32 @@
+package org.jembi.jempi.shared.kafka.globalContext.globalKTableWrapper;
+
+import org.apache.kafka.streams.processor.api.Processor;
+import org.apache.kafka.streams.processor.api.Record;
+import org.apache.kafka.streams.processor.api.ProcessorContext;
+import org.apache.kafka.streams.state.KeyValueStore;
+
+public class GlobalKTableValuesUpdater<T> implements Processor<String, T, String, T> {
+
+    private ProcessorContext<String, T> context;
+    private KeyValueStore<String, T> globalStore;
+    private final TableUpdaterProcessor<T, T, T> valuesUpdater;
+    private final String globalStoreName;
+
+    public GlobalKTableValuesUpdater(final TableUpdaterProcessor<T, T, T> valuesUpdater, final String globalStoreName){
+        this.valuesUpdater = valuesUpdater;
+        this.globalStoreName = globalStoreName;
+
+    }
+    @Override
+    public void init(ProcessorContext<String, T> context) {
+        this.context = context;
+        this.globalStore = context.getStateStore(globalStoreName);
+    }
+
+    @Override
+    public void process(final Record<String, T> record) {
+        this.globalStore.put(record.key(), record.value());
+        this.context.forward(record);
+    }
+
+}
--- JeMPI_Apps/JeMPI_LibShared/src/main/java/org/jembi/jempi/shared/kafka/globalContext/globalKTableWrapper/GlobalKTableWrapperInstance.java
+++ JeMPI_Apps/JeMPI_LibShared/src/main/java/org/jembi/jempi/shared/kafka/globalContext/globalKTableWrapper/GlobalKTableWrapperInstance.java
@@ -1,27 +1,20 @@
 package org.jembi.jempi.shared.kafka.globalContext.globalKTableWrapper;
 
 import org.apache.kafka.common.serialization.Serdes;
-import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
-import org.apache.kafka.streams.KafkaStreams;
-import org.apache.kafka.streams.StoreQueryParameters;
-import org.apache.kafka.streams.StreamsBuilder;
-import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.*;
 import org.apache.kafka.streams.errors.InvalidStateStoreException;
 import org.apache.kafka.streams.errors.StreamsUncaughtExceptionHandler;
 import org.apache.kafka.streams.kstream.Consumed;
-import org.apache.kafka.streams.kstream.GlobalKTable;
-import org.apache.kafka.streams.kstream.Materialized;
+import org.apache.kafka.streams.processor.api.Processor;
+import org.apache.kafka.streams.processor.api.ProcessorSupplier;
 import org.apache.kafka.streams.state.*;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.jembi.jempi.shared.kafka.MyKafkaConsumerByPartition;
 import org.jembi.jempi.shared.kafka.MyKafkaProducer;
-import org.jembi.jempi.shared.kafka.globalContext.globalKTableWrapper.serde.KTableDeserializer;
 import org.jembi.jempi.shared.kafka.globalContext.globalKTableWrapper.serde.KTableSerde;
 import org.jembi.jempi.shared.kafka.globalContext.globalKTableWrapper.serde.KTableSerializer;
 
-import java.time.Duration;
 import java.util.Properties;
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
@@ -36,9 +29,9 @@ public class GlobalKTableWrapperInstance<T> {
     private static final Logger LOGGER = LogManager.getLogger(GlobalKTableWrapper.class);
     private final String topicName;
     private final String uniqueId;
+    private final String globalStoreName;
     private final ReadOnlyKeyValueStore<String, T> keyValueStore;
     private final MyKafkaProducer<String, T> updater;
-    private final GlobalKTable<String, T> globalTable;
     private final Class<T> serializeCls;
     KafkaStreams streams;
     GlobalKTableWrapperInstance(final String bootStrapServers, final String topicName,  Class<T> serializeCls) throws InterruptedException, ExecutionException {
@@ -46,14 +39,24 @@ public class GlobalKTableWrapperInstance<T> {
         this.serializeCls = serializeCls;
         this.topicName = topicName;
         this.uniqueId = getUniqueId(topicName);
+        this.globalStoreName = String.format("%s-store", topicName);
 
         StreamsBuilder builder = new StreamsBuilder();
+        builder.addGlobalStore(Stores.keyValueStoreBuilder(
+                        Stores.persistentKeyValueStore(globalStoreName), // TODO: Consider in memory. Also consider umique name
+                        Serdes.String(),
+                        new KTableSerde<T>(this.serializeCls))
+                ,
+                topicName,
+                Consumed.with(Serdes.String(),  new KTableSerde<T>(this.serializeCls)),
+                new ProcessorSupplier<String, T, Void, Void>() {
+                    public Processor<String, T, Void, Void> get() {
+                        return new GlobalKTableValuesUpdater(null, globalStoreName);
+                    }
+                });
 
-        globalTable = builder.globalTable(topicName,
-                                          Consumed.with(Serdes.String(),  new KTableSerde<T>(this.serializeCls)),
-                                          Materialized.as(String.format("%s-store", topicName)));
 
-        streams = new KafkaStreams(builder.build(), this.getProperties(bootStrapServers, uniqueId));
+        streams = new KafkaStreams(builder.build(), this.getProperties(bootStrapServers, uniqueId)); // TODO: Everytime called creates own unique
 
         streams.setUncaughtExceptionHandler(( exception) -> {
             LOGGER.error(String.format("A error occurred on the global KTable stream %s", topicName), exception);
@@ -62,17 +65,17 @@ public class GlobalKTableWrapperInstance<T> {
 
         streams.start();
 
-
-        keyValueStore = streams.store(StoreQueryParameters.fromNameAndType(String.format("%s-store", topicName),
+        keyValueStore = streams.store(StoreQueryParameters.fromNameAndType(globalStoreName,
                                         QueryableStoreTypes.keyValueStore()));
-        waitUntilStoreIsQueryable(streams).get();
 
+        waitUntilStoreIsQueryable(streams).get();
 
         updater = new MyKafkaProducer(bootStrapServers,
                                         topicName,
                                         new StringSerializer(),
                                         new KTableSerializer<T>(),
                                         String.format("%s-producer", getUniqueId(topicName)));
+
         Runtime.getRuntime().addShutdownHook(new Thread(streams::close));
     }
 
@@ -112,8 +115,6 @@ public class GlobalKTableWrapperInstance<T> {
         Properties properties = new Properties();
         properties.put(StreamsConfig.APPLICATION_ID_CONFIG, String.format("%s-app.id", uniqueName));
         properties.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, bootStrapServers);
-        properties.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
-        properties.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, new KTableSerde<T>(this.serializeCls).getClass());
 
         return properties;
     }
@@ -130,33 +131,5 @@ public class GlobalKTableWrapperInstance<T> {
 
     }
 
-    public void updateValueToTopicEvent(final String bootstrapServers,
-                                        final String topic,
-                                        final String clientId,
-                                        final String groupId,
-                                        final TableUpdaterProcessor<T, T, T> processor){
 
-        // TODO maybe listen on global store/all patitions
-        // TODO: maybe use the process api that listen to all table and update, it wont change
-        MyKafkaConsumerByPartition<String, T> p = new MyKafkaConsumerByPartition<String, T>(
-                bootstrapServers,
-                topic,
-                new StringDeserializer(),
-                new KTableDeserializer<T>(this.serializeCls),
-                clientId,
-                groupId,
-                500,
-                10
-                );
-
-        p.poll(Duration.ofMillis(200)).forEach(record -> {
-            try {
-                updateValue(processor.apply(getValue(), record.value()));
-            } catch (ExecutionException | InterruptedException e) {
-                LOGGER.error(String.format("Failing to update the global kTable %s with the record %s", topicName, ""), e); //TODO
-                throw new RuntimeException(e);
-            }
-        });
-
-    }
 }
