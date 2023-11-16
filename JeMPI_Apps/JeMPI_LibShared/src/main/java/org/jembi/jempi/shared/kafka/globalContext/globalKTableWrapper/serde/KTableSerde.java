a808ae3723bf07e9937152ae2b046fd591586ef1
ChidoW <warambwac@gmail.com>
2023-11-16 09:13:05 +0200

    Updating Kafka global tables

--- JeMPI_Apps/JeMPI_LibShared/src/main/java/org/jembi/jempi/shared/kafka/globalContext/globalKTableWrapper/serde/KTableSerde.java
+++ JeMPI_Apps/JeMPI_LibShared/src/main/java/org/jembi/jempi/shared/kafka/globalContext/globalKTableWrapper/serde/KTableSerde.java
@@ -3,6 +3,7 @@ package org.jembi.jempi.shared.kafka.globalContext.globalKTableWrapper.serde;
 import org.apache.kafka.common.serialization.Serdes;
 
 public class KTableSerde<T> extends Serdes.WrapperSerde<T> {
+
     public KTableSerde(Class<T> serializeCls) {
         super(new KTableSerializer<T>(), new KTableDeserializer<T>(serializeCls));
     }
