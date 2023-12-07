1b848e827a10781fa51f2a558e36f95935c65ff4
Ajeenckya Gadewar <ajinkya.gadewar@jembi.org>
2023-12-07 14:11:25 +0530

    Added script for DGraph

--- devops/linux/docker/generate_logrotate_conf.sh
+++ devops/linux/docker/generate_logrotate_conf.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Load Backup Directory from Environment Variable
-BACKUP_PATH="${POSTGRES_BACKUP_DIRECTORY}"
+POSTGRES_BACKUP_PATH="${POSTGRES_BACKUP_DIRECTORY}"
 OLD_LOGS_DIR="${BACKUP_PATH}/old_logs"
 
 # Create Logrotate Configuration File
 cat << EOF > /etc/logrotate.d/postgres-backup
-$BACKUP_PATH/backup_*.log {
+$POSTGRES_BACKUP_PATH/backup_*.log {
     rotate 30
     daily
     missingok
@@ -21,3 +21,24 @@ $BACKUP_PATH/backup_*.log {
 EOF
 
 echo "Logrotate configuration for PostgreSQL backup updated."
+
+DGRAPH_BACKUP_PATH="${DGRAPH_BACKUP_DIRECTORY}"
+OLD_LOGS_DIR="${BACKUP_PATH}/old_logs"
+
+# Create Logrotate Configuration File
+cat << EOF > /etc/logrotate.d/dgraph-backup
+$DGRAPH_BACKUP_PATH/backup_*.log {
+    rotate 30
+    daily
+    missingok
+    notifempty
+    compress
+    delaycompress
+    create 640 root adm
+    dateext
+    dateformat -%Y%m%d
+    olddir $OLD_LOGS_DIR
+}
+EOF
+
+echo "Logrotate configuration for DGraph backup updated."
