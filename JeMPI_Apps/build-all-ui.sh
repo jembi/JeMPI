5b029f53c962b10524dea3f6a835c3f0720df308
Ryan Crichton <ryan@jembi.org>
2023-12-08 12:21:37 +0200

    Improve build scripts

--- JeMPI_Apps/build-all-ui.sh
+++ JeMPI_Apps/build-all-ui.sh
@@ -4,9 +4,14 @@ set -e
 set -u
 
 pushd JeMPI_UI
-  ./build-image.sh || exit 1
+./build-image.sh || exit 1
 popd
 
+if [ -z "$IMAGE_REGISTRY" ]; then
+  # If using local registry stop here, no need to push
+  return
+fi
+
 pushd JeMPI_UI
-  ./push.sh
+./push.sh
 popd
