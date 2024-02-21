531b91c613dc164a334b5aafcbb106b6b58cf75c
ChidoW <warambwac@gmail.com>
2024-02-21 16:43:37 +0200

    pdating build ci logic

--- JeMPI_Apps/build-all-ci.sh
+++ JeMPI_Apps/build-all-ci.sh
@@ -11,8 +11,10 @@ fi
 
 tag_image() {
     if [ ! -z "$tag_to_use" ]; then
-        IFS=':' read -a image_details <<< "$1"
+        IFS=':' read -a image_details <<< "$2"
         docker tag $1 ${image_details[0]}:$tag_to_use
+    else
+        docker tag $1 $2
     fi
 }
 
@@ -31,37 +33,37 @@ popd
 
 pushd JeMPI_AsyncReceiver
   ./build.sh || exit 1
-  tag_image $ASYNC_RECEIVER_HUB_IMAGE
+  tag_image $ASYNC_RECEIVER_IMAGE $ASYNC_RECEIVER_HUB_IMAGE
 popd
 pushd JeMPI_ETL
   ./build.sh || exit 1
-  tag_image $ETL_HUB_IMAGE
+  tag_image $ETL_IMAGE $ETL_HUB_IMAGE
 popd
 pushd JeMPI_Controller
   ./build.sh || exit 1
-  tag_image $CONTROLLER_HUB_IMAGE
+  tag_image $CONTROLLER_IMAGE $CONTROLLER_HUB_IMAGE
 popd
 pushd JeMPI_EM_Scala
   ./build.sh || exit 1
-  tag_image $EM_SCALA_HUB_IMAGE
+  tag_image $EM_SCALA_IMAGE $EM_SCALA_HUB_IMAGE
 popd
 pushd JeMPI_Linker
   ./build.sh || exit 1
-  tag_image $LINKER_HUB_IMAGE
+  tag_image $LINKER_IMAGE $LINKER_HUB_IMAGE
 popd
 pushd JeMPI_API
   ./build.sh || exit 1
-   tag_image $API_HUB_IMAGE
+   tag_image $API_IMAGE $API_HUB_IMAGE
 popd
 pushd JeMPI_API_KC
   ./build.sh || exit 1
-   tag_image $API_KC_HUB_IMAGE
+   tag_image $API_KC_IMAGE $API_KC_HUB_IMAGE
 popd
 pushd JeMPI_Bootstrapper
   ./build.sh || exit 1
-  tag_image $BOOTSTRAPPER_HUB_IMAGE
+  tag_image $BOOTSTRAPPER_IMAGE $BOOTSTRAPPER_HUB_IMAGE
 popd
 pushd JeMPI_UI
   ./build-image.sh || exit 1
-  tag_image $UI_HUB_IMAGE
+  tag_image $UI_IMAGE $UI_HUB_IMAGE
 popd
