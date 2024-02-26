c609e7b0e9ed99c2e5f598f0572324cb412b071e
NyashaMuusha <nyashamuusha@gmail.com>
2024-02-26 15:23:08 +0200

    refactored audit schema

--- JeMPI_Apps/JeMPI_Bootstrapper/src/main/resources/data/postgres/audit-schema.sql
+++ JeMPI_Apps/JeMPI_Bootstrapper/src/main/resources/data/postgres/audit-schema.sql
@@ -6,31 +6,9 @@ CREATE TABLE IF NOT EXISTS audit_trail (
     goldenID       VARCHAR(64),
     event          VARCHAR(256),
     score          FLOAT DEFAULT -1.0,
-    linkingRule    VARCHAR(14),
+    linkingRule    VARCHAR(14) DEFAULT 'UNMATCHED',
     CONSTRAINT PKEY_AUDIT_TRAIL PRIMARY KEY (id)
 );
+
 CREATE INDEX IF NOT EXISTS idx_gid ON audit_trail(goldenID);
 CREATE INDEX IF NOT EXISTS idx_iid ON audit_trail(interactionID);
-
-DO $$ 
-BEGIN 
-    IF NOT EXISTS (SELECT 1 FROM information_schema.columns WHERE table_name = 'audit_trail' AND column_name = 'score') THEN 
-        ALTER TABLE audit_trail ADD COLUMN score FLOAT;
-    END IF; 
-END $$;
-
-UPDATE audit_trail
-SET score = -1.0
-WHERE score IS NULL;
-
-DO $$
-BEGIN
-    IF NOT EXISTS (SELECT 1 FROM information_schema.columns WHERE table_name = 'audit_trail' AND column_name = 'linkingrule') THEN
-        ALTER TABLE audit_trail ADD COLUMN linkingRule VARCHAR(14);
-    END IF;
-END $$;
-
-UPDATE audit_trail
-SET linkingRule = 'UNMATCHED'
-WHERE linkingRule IS NULL;
-
