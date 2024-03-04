c8d500cecd0e15ac00bc9ee2ce0280572ef323bd
Sushant Patil <43531186+sushantpatil1214@users.noreply.github.com>
2024-03-04 12:07:03 +0530

    Added Automation script for windows installation (#188)
    * Added Automation script for windows installation
    
    * Added Automation script for windows installation
    
    * Added Automation script for windows installation
    
    * Commented output file

---
+++ devops/windows/base-docker-linux/z-stack-2-reboot-hub-images-db-init.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+set -u
+
+echo
+echo "Down stack"
+source ./helper/scripts/d-stack-08-rm.sh
+source ./helper/scripts/d-stack-09-wait-removed.sh
+
+echo
+echo "Up app containers"
+source ./helper/scripts/d-stack-01-create-dirs.sh
+sleep 2
+source ./helper/scripts/d-stack-02-deploy-0.sh
+sleep 2
+source ./helper/scripts/d-stack-03-up-hub-containers.sh
+
