982c9d08f07f53a814d69c03d7760847e7128ef8
w-hayes <54031103+w-hayes@users.noreply.github.com>
2024-03-06 08:01:05 +0200

    Dev extended em (#195)
    * bounce scala lib version
    
    * fixed EM for link fields
    
    * removed unused import

--- JeMPI_Apps/JeMPI_EM_Scala/src/main/scala/org/jembi/jempi/em/CustomFields.scala
+++ JeMPI_Apps/JeMPI_EM_Scala/src/main/scala/org/jembi/jempi/em/CustomFields.scala
@@ -4,14 +4,40 @@ import scala.collection.immutable.ArraySeq
 
 object CustomFields {
 
+  private val COL_GIVEN_NAME = 0
+  private val COL_FAMILY_NAME = 1
+  private val COL_GENDER = 2
+  private val COL_DOB = 3
+  private val COL_CITY = 4
+  private val COL_PHONE_NUMBER = 5
+  private val COL_NATIONAL_ID = 6
+
   val FIELDS: ArraySeq[Field] = ArraySeq(
-    Field("givenName", 0),
-    Field("familyName", 1),
-    Field("gender", 2),
-    Field("dob", 3),
-    Field("city", 4),
-    Field("phoneNumber", 5),
-    Field("nationalId", 6)
+    Field("givenName", COL_GIVEN_NAME),
+    Field("familyName", COL_FAMILY_NAME),
+    Field("gender", COL_GENDER),
+    Field("dob", COL_DOB),
+    Field("city", COL_CITY),
+    Field("phoneNumber", COL_PHONE_NUMBER),
+    Field("nationalId", COL_NATIONAL_ID)
+  )
+
+  val LINK_COLS: ArraySeq[Int] = ArraySeq(
+    COL_GIVEN_NAME,
+    COL_FAMILY_NAME,
+    COL_GENDER,
+    COL_DOB,
+    COL_CITY,
+    COL_PHONE_NUMBER,
+    COL_NATIONAL_ID
+  )
+
+  val VALIDATE_COLS: ArraySeq[Int] = ArraySeq(
+
+  )
+
+  val MATCH_COLS: ArraySeq[Int] = ArraySeq(
+
   )
 
 }
--- JeMPI_Apps/JeMPI_EM_Scala/src/main/scala/org/jembi/jempi/em/Tallies.scala
+++ JeMPI_Apps/JeMPI_EM_Scala/src/main/scala/org/jembi/jempi/em/Tallies.scala
@@ -1,6 +1,5 @@
 package org.jembi.jempi.em
 
-import CustomFields.FIELDS
 import scala.collection.immutable.ArraySeq
 
-case class Tallies(colTally: ArraySeq[Tally] = FIELDS.map(_ => Tally()))
+case class Tallies(colTally: ArraySeq[Tally] = CustomFields.LINK_COLS.map(_ => Tally()))
