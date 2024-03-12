f57fa009a4df8c46569447da1830cdf6d7f453da
NyashaMuusha <nyashamuusha@gmail.com>
2024-03-12 10:24:55 +0200

    fixed formatting

--- JeMPI_Apps/JeMPI_LibShared/src/main/java/org/jembi/jempi/shared/models/ApiModels.java
+++ JeMPI_Apps/JeMPI_LibShared/src/main/java/org/jembi/jempi/shared/models/ApiModels.java
@@ -35,14 +35,14 @@ public abstract class ApiModels {
 
    public record ApiError(
 
-           @JsonProperty("module") String module,
-           @JsonProperty("class") String klass,
-           @JsonProperty("line_number") Integer lineNumber) {
+        @JsonProperty("module") String module,
+        @JsonProperty("class") String klass,
+        @JsonProperty("line_number") Integer lineNumber) {
 
       public ApiError() {
          this(Thread.currentThread().getStackTrace()[3].getModuleName(),
-                 Thread.currentThread().getStackTrace()[3].getClassName(),
-                 Thread.currentThread().getStackTrace()[3].getLineNumber());
+              Thread.currentThread().getStackTrace()[3].getClassName(),
+              Thread.currentThread().getStackTrace()[3].getLineNumber());
       }
 
 
@@ -55,15 +55,15 @@ public abstract class ApiModels {
    }
 
    public record ApiSearchParameter(
-           String value,
-           String fieldName,
-           Integer distance) {
+         String value,
+         String fieldName,
+         Integer distance) {
    }
 
    @JsonInclude(JsonInclude.Include.NON_NULL)
    public record ApiCrCandidatesRequest(
-           Float candidateThreshold,
-           CustomDemographicData demographicData) {
+         Float candidateThreshold,
+         CustomDemographicData demographicData) {
    }
 
    @JsonInclude(JsonInclude.Include.NON_NULL)
@@ -72,14 +72,14 @@ public abstract class ApiModels {
 
    @JsonInclude(JsonInclude.Include.NON_NULL)
    public record ApiCrFindRequest(
-           ApiOperand operand,
-           List<ApiLogicalOperand> operands) {
+         ApiOperand operand,
+         List<ApiLogicalOperand> operands) {
       @JsonInclude(JsonInclude.Include.NON_NULL)
       public record ApiOperand(
-              String fn,
-              Long distance,
-              String name,
-              String value) {
+             String fn,
+             Long distance,
+             String name,
+             String value) {
       }
 
       public record ApiLogicalOperand(
@@ -96,21 +96,21 @@ public abstract class ApiModels {
 
    @JsonInclude(JsonInclude.Include.NON_NULL)
    public record ApiCrRegisterRequest(
-           Float candidateThreshold,
-           CustomSourceId sourceId,
-           CustomUniqueInteractionData uniqueInteractionData,
-           CustomDemographicData demographicData) {
+          Float candidateThreshold,
+          CustomSourceId sourceId,
+          CustomUniqueInteractionData uniqueInteractionData,
+          CustomDemographicData demographicData) {
    }
 
 
    @JsonInclude(JsonInclude.Include.NON_NULL)
    public record LinkInteractionSyncBody(
-           String stan,
-           ExternalLinkRange externalLinkRange,
-           Float matchThreshold,
-           CustomSourceId sourceId,
-           CustomUniqueInteractionData uniqueInteractionData,
-           CustomDemographicData demographicData) {
+          String stan,
+          ExternalLinkRange externalLinkRange,
+          Float matchThreshold,
+          CustomSourceId sourceId,
+          CustomUniqueInteractionData uniqueInteractionData,
+          CustomDemographicData demographicData) {
    }
 
    @JsonInclude(JsonInclude.Include.NON_NULL)
@@ -129,38 +129,38 @@ public abstract class ApiModels {
 
    @JsonInclude(JsonInclude.Include.NON_NULL)
    public record ApiCrUpdateFieldsRequest(
-           String goldenId,
-           List<ApiCrUpdateField> fields) {
+          String goldenId,
+          List<ApiCrUpdateField> fields) {
 
       public record ApiCrUpdateField(
-              String name,
-              String value) {
+             String name,
+             String value) {
       }
 
    }
 
    @JsonInclude(JsonInclude.Include.NON_NULL)
    public record ApiCrUpdateFieldsResponse(
-           String goldenId,
-           List<String> updated,
-           List<String> failed) {
+          String goldenId,
+          List<String> updated,
+          List<String> failed) {
    }
 
 
    @JsonInclude(JsonInclude.Include.NON_NULL)
    public record ApiSimpleSearchRequestPayload(
-           List<ApiSearchParameter> parameters,
-           Integer offset,
-           Integer limit,
-           String sortBy,
-           Boolean sortAsc) {
+          List<ApiSearchParameter> parameters,
+          Integer offset,
+          Integer limit,
+          String sortBy,
+          Boolean sortAsc) {
 
       public ApiSimpleSearchRequestPayload(
-              final List<ApiSearchParameter> parameters,
-              final Integer offset,
-              final Integer limit,
-              final String sortBy,
-              final Boolean sortAsc) {
+             final List<ApiSearchParameter> parameters,
+             final Integer offset,
+             final Integer limit,
+             final String sortBy,
+             final Boolean sortAsc) {
          this.parameters = ObjectUtils.defaultIfNull(parameters, new ArrayList<>());
          this.offset = ObjectUtils.defaultIfNull(offset, 0);
          this.limit = ObjectUtils.defaultIfNull(limit, 10);
@@ -184,41 +184,41 @@ public abstract class ApiModels {
    }
 
    public record ApiExpandedGoldenRecordsPaginatedResultSet(
-           List<ApiExpandedGoldenRecord> data,
-           ApiPagination pagination) implements ApiPaginatedResultSet {
+          List<ApiExpandedGoldenRecord> data,
+          ApiPagination pagination) implements ApiPaginatedResultSet {
       public static ApiExpandedGoldenRecordsPaginatedResultSet fromLibMPIPaginatedResultSet(
-              final LibMPIPaginatedResultSet<ExpandedGoldenRecord> resultSet) {
+             final LibMPIPaginatedResultSet<ExpandedGoldenRecord> resultSet) {
          final var data = resultSet.data().stream().map(ApiExpandedGoldenRecord::fromExpandedGoldenRecord).toList();
          return new ApiExpandedGoldenRecordsPaginatedResultSet(data, ApiPagination.fromLibMPIPagination(resultSet.pagination()));
       }
    }
 
    public record ApiInteractionsPaginatedResultSet(
-           List<ApiInteraction> data,
-           ApiPagination pagination) implements ApiPaginatedResultSet {
+          List<ApiInteraction> data,
+          ApiPagination pagination) implements ApiPaginatedResultSet {
       public static ApiInteractionsPaginatedResultSet fromLibMPIPaginatedResultSet(
-              final LibMPIPaginatedResultSet<Interaction> resultSet) {
+             final LibMPIPaginatedResultSet<Interaction> resultSet) {
          final var data = resultSet.data().stream().map(ApiInteraction::fromInteraction).toList();
          return new ApiInteractionsPaginatedResultSet(data, ApiPagination.fromLibMPIPagination(resultSet.pagination()));
       }
    }
 
    public record ApiFiteredGidsPaginatedResultSet(
-           List<String> data,
-           ApiPagination pagination) implements ApiPaginatedResultSet {
+          List<String> data,
+          ApiPagination pagination) implements ApiPaginatedResultSet {
       public static ApiFiteredGidsPaginatedResultSet fromLibMPIPaginatedResultSet(
-              final LibMPIPaginatedResultSet<String> resultSet) {
+             final LibMPIPaginatedResultSet<String> resultSet) {
          final var data = resultSet.data().stream().toList();
          return new ApiFiteredGidsPaginatedResultSet(data, ApiPagination.fromLibMPIPagination(resultSet.pagination()));
       }
    }
 
    public record ApiFiteredGidsWithInteractionCountPaginatedResultSet(
-           List<String> data,
-           InteractionCount interactionCount,
-           ApiPagination pagination) implements ApiPaginatedResultSet {
+          List<String> data,
+          InteractionCount interactionCount,
+          ApiPagination pagination) implements ApiPaginatedResultSet {
       public static ApiFiteredGidsWithInteractionCountPaginatedResultSet fromPaginatedGidsWithInteractionCount(
-              final PaginatedGIDsWithInteractionCount resultSet) {
+             final PaginatedGIDsWithInteractionCount resultSet) {
          final var data = resultSet.data().stream().toList();
          return new ApiFiteredGidsWithInteractionCountPaginatedResultSet(data,
                  InteractionCount.fromInteractionCount(resultSet.interactionCount()),
@@ -228,10 +228,10 @@ public abstract class ApiModels {
 
    @JsonInclude(JsonInclude.Include.NON_NULL)
    public record ApiGoldenRecord(
-           String uid,
-           List<CustomSourceId> sourceId,
-           CustomUniqueGoldenRecordData uniqueGoldenRecordData,
-           CustomDemographicData demographicData) {
+          String uid,
+          List<CustomSourceId> sourceId,
+          CustomUniqueGoldenRecordData uniqueGoldenRecordData,
+          CustomDemographicData demographicData) {
       static ApiGoldenRecord fromGoldenRecord(final GoldenRecord goldenRecord) {
          return new ApiGoldenRecord(goldenRecord.goldenId(),
                  goldenRecord.sourceId(),
@@ -242,8 +242,8 @@ public abstract class ApiModels {
 
    @JsonInclude(JsonInclude.Include.NON_NULL)
    public record ApiGoldenRecordWithScore(
-           ApiGoldenRecord goldenRecord,
-           Float score) {
+          ApiGoldenRecord goldenRecord,
+          Float score) {
       static ApiGoldenRecordWithScore fromGoldenRecordWithScore(final GoldenRecordWithScore goldenRecordWithScore) {
          return new ApiGoldenRecordWithScore(ApiGoldenRecord.fromGoldenRecord(goldenRecordWithScore.goldenRecord()),
                  goldenRecordWithScore.score());
@@ -251,8 +251,8 @@ public abstract class ApiModels {
    }
 
    public record ApiExpandedGoldenRecord(
-           ApiGoldenRecord goldenRecord,
-           List<ApiInteractionWithScore> interactionsWithScore) {
+          ApiGoldenRecord goldenRecord,
+          List<ApiInteractionWithScore> interactionsWithScore) {
       public static ApiExpandedGoldenRecord fromExpandedGoldenRecord(final ExpandedGoldenRecord expandedGoldenRecord) {
          return new ApiExpandedGoldenRecord(ApiGoldenRecord.fromGoldenRecord(expandedGoldenRecord.goldenRecord()),
                  expandedGoldenRecord.interactionsWithScore()
@@ -263,8 +263,8 @@ public abstract class ApiModels {
    }
 
    public record ApiExpandedInteraction(
-           ApiInteraction interaction,
-           List<ApiGoldenRecordWithScore> goldenRecordsWithScore) {
+          ApiInteraction interaction,
+          List<ApiGoldenRecordWithScore> goldenRecordsWithScore) {
       public static ApiExpandedInteraction fromExpandedInteraction(final ExpandedInteraction expandedInteraction) {
          return new ApiExpandedInteraction(ApiInteraction.fromInteraction(expandedInteraction.interaction()),
                  expandedInteraction.goldenRecordsWithScore()
@@ -276,10 +276,10 @@ public abstract class ApiModels {
 
    @JsonInclude(JsonInclude.Include.NON_NULL)
    public record ApiInteraction(
-           String uid,
-           CustomSourceId sourceId,
-           CustomUniqueInteractionData uniqueInteractionData,
-           CustomDemographicData demographicData) {
+          String uid,
+          CustomSourceId sourceId,
+          CustomUniqueInteractionData uniqueInteractionData,
+          CustomDemographicData demographicData) {
       public static ApiInteraction fromInteraction(final Interaction interaction) {
          return new ApiInteraction(interaction.interactionId(),
                  interaction.sourceId(),
@@ -290,8 +290,8 @@ public abstract class ApiModels {
 
    @JsonInclude(JsonInclude.Include.NON_NULL)
    public record ApiInteractionWithScore(
-           ApiInteraction interaction,
-           Float score) {
+          ApiInteraction interaction,
+          Float score) {
       static ApiInteractionWithScore fromPatientRecordWithScore(final InteractionWithScore interactionWithScore) {
          return new ApiInteractionWithScore(ApiInteraction.fromInteraction(interactionWithScore.interaction()),
                  interactionWithScore.score());
@@ -299,8 +299,8 @@ public abstract class ApiModels {
    }
 
    public record ApiNumberOfRecords(
-           Long goldenRecords,
-           Long interactions) {
+          Long goldenRecords,
+          Long interactions) {
    }
 
    @JsonInclude(JsonInclude.Include.NON_NULL)
@@ -309,37 +309,37 @@ public abstract class ApiModels {
 
       @JsonInclude(JsonInclude.Include.NON_NULL)
       public record LinkingAuditEntry(
-              @JsonProperty("inserted_at") String insertedAt,
-              @JsonProperty("created_at") String createdAt,
-              @JsonProperty("interaction_id") String interactionId,
-              @JsonProperty("golden_id") String goldenId,
-              @JsonProperty("entry") String entry,
-              @JsonProperty("score") Float score,
-              @JsonProperty("linking_rule") String linkingRule
+             @JsonProperty("inserted_at") String insertedAt,
+             @JsonProperty("created_at") String createdAt,
+             @JsonProperty("interaction_id") String interactionId,
+             @JsonProperty("golden_id") String goldenId,
+             @JsonProperty("entry") String entry,
+             @JsonProperty("score") Float score,
+             @JsonProperty("linking_rule") String linkingRule
       ) {
       }
    }
 
    public record ApiCalculateScoresRequest(
-           String interactionId,
-           List<String> goldenIds) {
+          String interactionId,
+          List<String> goldenIds) {
    }
 
    public record ApiCalculateScoresResponse(
-           String interactionId,
-           List<ApiScore> scores) {
+          String interactionId,
+          List<ApiScore> scores) {
 
       public record ApiScore(
-              String goldenId,
-              float score) {
+             String goldenId,
+             float score) {
       }
 
    }
 
    public record ApiExtendedLinkInfo(
-           String stan,
-           LinkInfo linkInfo,
-           List<ExternalLinkCandidate> externalLinkCandidateList) {
+          String stan,
+          LinkInfo linkInfo,
+          List<ExternalLinkCandidate> externalLinkCandidateList) {
    }
 
 }
