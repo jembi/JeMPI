3a25ba74ad2272bd63c019f66a9d005359c09c63
Nyasha Ivy Peace <92095336+NyashaMuusha@users.noreply.github.com>
2024-04-03 18:10:02 +0200

    Update index.ts

--- JeMPI_Apps/JeMPI_UI/src/config/index.ts
+++ JeMPI_Apps/JeMPI_UI/src/config/index.ts
@@ -54,13 +54,13 @@ export default async function getConfig() {
       ),
       showBrandLogo: process.env.REACT_APP_SHOW_BRAND_LOGO === 'true',
       refetchInterval: +(
-        process.env.REACT_APP_REFETCH_INTERVAL || 5 * 60 * 1000
+        process.env.REACT_APP_REFETCH_INTERVAL || 3000
       ),
       cacheTime: +(
-        process.env.REACT_APP_CACHE_TIME || 5 * 60 * 1000
+        process.env.REACT_APP_CACHE_TIME || 3000
       ),
       staleTime: +(
-        process.env.REACT_APP_STALE_TIME || 5 * 60 * 1000
+        process.env.REACT_APP_STALE_TIME || 3000
       ),
     }
   }
