d0a281967b81d45fe7070a0f9504e6074155da0c
NyashaMuusha <nyashamuusha@gmail.com>
2024-04-11 23:11:48 +0200

    rebamed endpoints

--- JeMPI_Apps/JeMPI_UI/src/services/apiRoutes.ts
+++ JeMPI_Apps/JeMPI_UI/src/services/apiRoutes.ts
@@ -1,6 +1,6 @@
 const ROUTES = {
   POST_FIELDS_CONFIG: '/config',
-  GET_LINKED_RECORDS: '/linkedRecords',
+  POST_LINKED_RECORDS: '/linkedRecords',
   POST_NOTIFICATIONS: '/matchesForReview',
   POST_INTERACTION: '/interaction',
   POST_DASHBOARD_DATA: '/dashboardData',
@@ -15,8 +15,8 @@ const ROUTES = {
   POST_FILTER_GIDS: '/filterGids',
   POST_FILTER_GIDS_WITH_INTERACTION_COUNT: '/filterGidsInteraction',
   POST_CR_CANDIDATES: '/crCandidates',
-  POST_IID_NEW_GID_LINK: '/unLink',
-  POST_IID_GID_LINK: '/link',
+  POST_IID_NEW_GID_LINK: '/newLink',
+  POST_IID_GID_LINK: '/relink',
   PATCH_GOLDEN_RECORD: '/goldenRecord',
   CURRENT_USER: '/currentUser',
   VALIDATE_OAUTH: '/authenticate',
