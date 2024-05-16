0bd4c5cc84dacd7de61137dde373499ed1a120fe
Ryan Crichton <ryan@jembi.org>
2024-05-16 15:22:30 +0200

    Apply suggestions from code review
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- JeMPI_Apps/JeMPI_UI/build-image.sh
+++ JeMPI_Apps/JeMPI_UI/build-image.sh
@@ -11,7 +11,7 @@ rm -f ./.env
 envsubst < $PROJECT_DEVOPS_DIR/conf/ui/.env > ./.env
 
 if [ "$CI" = "true" ]; then
-    docker buildx build --tag jembi/$UI_HUB_IMAGE:$TAG --push --target $NODE_ENV-stage --platform linux/amd64,linux/arm64 --builder=container .
+    docker buildx build --tag "jembi/$UI_HUB_IMAGE:$TAG" --push --target "$NODE_ENV-stage" --platform "linux/amd64,linux/arm64" --builder=container .
 else
-    docker build --tag $UI_IMAGE --target $NODE_ENV-stage .
+    docker build --tag "$UI_IMAGE" --target "$NODE_ENV-stage" .
 fi
