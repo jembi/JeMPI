f24a170b8fda0a112e95eae8fe29b78da742a24e
NyashaMuusha <nyashamuusha@gmail.com>
2024-05-20 11:37:40 +0200

    added select fields to design view

--- JeMPI_Apps/JeMPI_UI/src/pages/settings/deterministic/deterministic.tsx
+++ JeMPI_Apps/JeMPI_UI/src/pages/settings/deterministic/deterministic.tsx
@@ -1,4 +1,4 @@
-import { AddOutlined } from '@mui/icons-material'
+import { AddOutlined } from '@mui/icons-material';
 import {
   Box,
   Button,
@@ -8,110 +8,185 @@ import {
   FormControl,
   IconButton,
   InputLabel,
+  MenuItem,
   Select,
+  SelectChangeEvent,
   Typography
-} from '@mui/material'
-import React from 'react'
+} from '@mui/material';
+import { useEffect, useState } from 'react';
+import { DemographicField } from 'types/Configuration';
+import { transformFieldName } from 'utils/helpers';
 
-const Deterministic = () => {
-  const [viewType, setViewType] = React.useState(0)
-  return (
-    <>
-      <Card sx={{ minWidth: 275 }}>
-        <CardContent
-          sx={{
-            width: '100%',
-            display: 'flex',
-            flexDirection: 'column',
-            gap: 6
-          }}
-        >
-          <Box
-            sx={{ mr: 'auto', display: 'flex', flexDirection: 'row', gap: 2 }}
-          >
-            <Button
-              variant="outlined"
-              size="medium"
-              onClick={() => setViewType(0)}
-            >
-              Design View
-            </Button>
-            <Button
-              variant="outlined"
-              size="medium"
-              onClick={() => setViewType(1)}
-            >
-              Source View
-            </Button>
-          </Box>
-          {viewType === 0 ? (
-            <Box
-              sx={{
-                display: 'flex',
-                flexDirection: 'row',
-                width: '100%',
-                justifyContent: 'center',
-                gap: 2
-              }}
-            >
-              <FormControl fullWidth>
-                <InputLabel id="demo-simple-select-label">
-                  Select Comparator Function
-                </InputLabel>
-                <Select
-                  labelId="demo-simple-select-label"
-                  id="demo-simple-select"
-                  label="Select Comparator Function"
-                ></Select>
-              </FormControl>
-              <FormControl fullWidth>
-                <InputLabel id="demo-simple-select-label">
-                  Select Field
-                </InputLabel>
-                <Select
-                  labelId="demo-simple-select-label"
-                  id="demo-simple-select"
-                  label="Select Field"
-                ></Select>
-              </FormControl>
-              <FormControl fullWidth>
-                <InputLabel id="demo-simple-select-label">
-                  Select Operator
-                </InputLabel>
-                <Select
-                  labelId="demo-simple-select-label"
-                  id="demo-simple-select"
-                  label="Select Operator"
-                ></Select>
-              </FormControl>
-            </Box>
-          ) : (
-            <Box
-              sx={{
-                width: '100%',
-                display: 'flex',
-                flexDirection: 'column',
-                gap: 2,
-                justifyContent: 'center',
-                alignItems: 'center'
-              }}
-            >
-              <Typography variant="h5">eq (National ID)</Typography>
-              Or
-              <Typography variant="h5">
-                eq (given name) and eq(family name, 3) and eq (phone number)
-              </Typography>
-            </Box>
-          )}
-        </CardContent>
-        <CardActions>
-          <IconButton aria-label="delete" size="small">
-            <AddOutlined fontSize="small" />
-          </IconButton>
-        </CardActions>
-      </Card>
-    </>
-  )
+interface DeterministicProps {
+  demographicData: DemographicField[];
 }
 
-export default Deterministic
+const options = [
+  { value: 0, label: 'Exact' },
+  { value: 1, label: 'Low Fuzziness' },
+  { value: 2, label: 'Medium Fuzziness' },
+  { value: 3, label: 'High Fuzziness' }
+];
+
+enum Operator {
+  AND = 'And',
+  OR = 'Or'
+}
+
+const Deterministic = ({ demographicData = [] }: DeterministicProps) => {
+  const [viewType, setViewType] = useState<number>(0);
+  const [selectedComparator, setSelectedComparator] = useState<number>(0);
+  const [selectedField, setSelectedField] = useState<string>('');
+  const [selectedOperator, setSelectedOperator] = useState<Operator | ''>('');
+
+  useEffect(() => {
+    const savedComparator = localStorage.getItem('selectedComparator');
+    const savedField = localStorage.getItem('selectedField');
+    const savedOperator = localStorage.getItem('selectedOperator');
+
+    if (savedComparator) setSelectedComparator(Number(savedComparator));
+    if (savedField) setSelectedField(savedField);
+    if (savedOperator) setSelectedOperator(savedOperator as Operator);
+  }, []);
+
+
+  useEffect(() => {
+    localStorage.setItem('selectedComparator', selectedComparator.toString());
+  }, [selectedComparator]);
+
+  useEffect(() => {
+    localStorage.setItem('selectedField', selectedField);
+  }, [selectedField]);
+
+  useEffect(() => {
+    localStorage.setItem('selectedOperator', selectedOperator);
+  }, [selectedOperator]);
+
+  const handleComparatorChange = (event: SelectChangeEvent<typeof selectedComparator>) => {
+    setSelectedComparator(event.target.value as number);
+  };
+
+  const handleFieldChange = (event: SelectChangeEvent<typeof selectedField>) => {
+    setSelectedField(event.target.value);
+  };
+
+  const handleOperatorChange = (event: SelectChangeEvent<typeof selectedOperator>) => {
+    setSelectedOperator(event.target.value as Operator);
+  };
+
+  return (
+    <Card sx={{ minWidth: 275 }}>
+      <CardContent
+        sx={{
+          width: '100%',
+          display: 'flex',
+          flexDirection: 'column',
+          gap: 6
+        }}
+      >
+        <Box sx={{ mr: 'auto', display: 'flex', flexDirection: 'row', gap: 2 }}>
+          <Button
+            variant="outlined"
+            size="medium"
+            onClick={() => setViewType(0)}
+          >
+            Design View
+          </Button>
+          <Button
+            variant="outlined"
+            size="medium"
+            onClick={() => setViewType(1)}
+          >
+            Source View
+          </Button>
+        </Box>
+        {viewType === 0 ? (
+          <Box
+            sx={{
+              display: 'flex',
+              flexDirection: 'row',
+              width: '100%',
+              justifyContent: 'center',
+              gap: 2
+            }}
+          >
+            <FormControl fullWidth>
+              <InputLabel id="select-comparator-label">
+                Select Comparator Function
+              </InputLabel>
+              <Select
+                labelId="select-comparator-label"
+                id="select-comparator"
+                value={selectedComparator}
+                label="Select Comparator Function"
+                onChange={handleComparatorChange}
+              > 
+                {options.map((option, index) => (
+                  <MenuItem value={option.value} key={index}>
+                    {option.label}
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+            <FormControl fullWidth>
+              <InputLabel id="select-field-label">Select Field</InputLabel>
+              <Select
+                labelId="select-field-label"
+                id="select-field"
+                value={selectedField}
+                label="Select Field"
+                onChange={handleFieldChange}
+              >
+                {Array.isArray(demographicData) && demographicData.map((field, index) => (
+                  <MenuItem key={index} value={field.fieldName}>
+                    {transformFieldName(field.fieldName)}
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+            <FormControl fullWidth>
+              <InputLabel id="select-operator-label">Select Operator</InputLabel>
+              <Select
+                labelId="select-operator-label"
+                id="select-operator"
+                value={selectedOperator}
+                label="Select Operator"
+                onChange={handleOperatorChange}
+              >
+                {Object.values(Operator).map((op, index) => (
+                  <MenuItem key={index} value={op}>
+                    {op}
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+          </Box>
+        ) : (
+          <Box
+            sx={{
+              width: '100%',
+              display: 'flex',
+              flexDirection: 'column',
+              gap: 2,
+              justifyContent: 'center',
+              alignItems: 'center'
+            }}
+          >
+            <Typography variant="h5">eq (National ID)</Typography>
+            Or
+            <Typography variant="h5">
+              eq (given name) and eq(family name, 3) and eq (phone number)
+            </Typography>
+          </Box>
+        )}
+      </CardContent>
+      <CardActions>
+        <IconButton aria-label="add" size="small">
+          <AddOutlined fontSize="small" />
+        </IconButton>
+      </CardActions>
+    </Card>
+  );
+};
+
+export default Deterministic;
