655854a1eb5020d32f1e718473edb124224880ef
NyashaMuusha <nyashamuusha@gmail.com>
2024-06-18 14:20:28 +0200

    added basic tabs

---
+++ JeMPI_Apps/JeMPI_UI/src/pages/settings/probabilistic/Probabilistic.tsx
@@ -0,0 +1,58 @@
+import { useState } from "react";
+import { Card, CardContent, Tab, Tabs } from "@mui/material";
+import { Box } from "@mui/system";
+import { a11yProps, CustomTabPanel } from "../deterministic/BasicTabs";
+import ProbabilisticContent from "./ProbabilisticContent";
+
+
+
+const Probabilistic = () => {
+    const [value, setValue] = useState(0)
+
+    const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+      setValue(newValue)
+    }
+    return (
+        <Card sx={{ minWidth: 275 }}>
+        <CardContent
+          sx={{
+            width: '100%',
+            display: 'flex',
+            flexDirection: 'column',
+            backgroundColor: '#f5f5f5'
+          }}
+        >
+          <Box sx={{ width: '100%', backgroundColor: '#f5f5f5' }}>
+            <Box>
+              <Tabs
+                value={value}
+                onChange={handleChange}
+                aria-label="basic tabs example"
+              >
+                <Tab label="Linking" {...a11yProps(0)} />
+                <Tab label="Validate" {...a11yProps(1)} />
+                <Tab label="Matching" {...a11yProps(2)} />
+              </Tabs>
+            </Box>
+            <CustomTabPanel value={value} index={0}>
+              <ProbabilisticContent
+               
+              />
+            </CustomTabPanel>
+            <CustomTabPanel value={value} index={1}>
+              <ProbabilisticContent
+                
+              />
+            </CustomTabPanel>
+            <CustomTabPanel value={value} index={2}>
+              <ProbabilisticContent
+              />
+            </CustomTabPanel>
+          </Box>
+        </CardContent>
+      </Card>
+
+    );
+};
+
+export default Probabilistic;
