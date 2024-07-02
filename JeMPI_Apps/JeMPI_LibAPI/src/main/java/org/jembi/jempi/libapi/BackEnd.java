7d4ef2a8b01ce42e274890a37581321530195ad4
Matthew Erispe <matthew.erispe@jembi.org>
2024-07-02 12:22:26 +0200

    remove copy and delete

--- JeMPI_Apps/JeMPI_LibAPI/src/main/java/org/jembi/jempi/libapi/BackEnd.java
+++ JeMPI_Apps/JeMPI_LibAPI/src/main/java/org/jembi/jempi/libapi/BackEnd.java
@@ -455,9 +455,7 @@ public final class BackEnd extends AbstractBehavior<BackEnd.Event> {
          if (request.uploadConfig != null) {
             appendUploadConfigToFile(request.uploadConfig, file);
          }
-         Files.copy(file.toPath(), Paths.get("/app/csv/" + file.getName()));
-         Files.move(Paths.get("/app/csv/" + file.getName()), Paths.get("/app/csv/" + file.getName().replace(".tmp", "")));
-         Files.delete(file.toPath());
+         Files.move(file.toPath(), Paths.get("/app/csv/" + file.getName().replace(".tmp", "")));
       } catch (NoSuchFileException e) {
          LOGGER.error("No such file");
       } catch (SecurityException | IOException e) {
