82e6c72c49623df7717087762f9f4e96e3c897bf
Sushant <sushantpatil1214@gmail.com>
2024-07-19 17:55:33 +0530

    Added variable for CI

--- JeMPI_Apps/build-all-java.sh
+++ JeMPI_Apps/build-all-java.sh
@@ -3,39 +3,41 @@
 set -e
 set -u
 
+CI=${CI:-false}
 if [ $# -eq 0 ]; then
     tag_to_use="ci-test-main" 
 else
     tag_to_use=$1
 fi
+
 mvn clean package
 pushd JeMPI_EM_Scala
   sbt clean assembly
 popd
 
 pushd JeMPI_AsyncReceiver
-  CI=false TAG=$tag_to_use ./build.sh || exit 1
+  $CI TAG=$tag_to_use ./build.sh || exit 1
 popd
 pushd JeMPI_ETL
-  CI=false TAG=$tag_to_use ./build.sh || exit 1
+  $CI TAG=$tag_to_use ./build.sh || exit 1
 popd
 pushd JeMPI_Controller
-  CI=false TAG=$tag_to_use ./build.sh || exit 1
+  $CI TAG=$tag_to_use ./build.sh || exit 1
 popd
 pushd JeMPI_EM_Scala
-  CI=false TAG=$tag_to_use ./build.sh || exit 1
+  $CI TAG=$tag_to_use ./build.sh || exit 1
 popd
 pushd JeMPI_Linker
-  CI=false TAG=$tag_to_use ./build.sh || exit 1
+  $CI TAG=$tag_to_use ./build.sh || exit 1
 popd
 pushd JeMPI_API
-  CI=false TAG=$tag_to_use ./build.sh || exit 1
+  $CI TAG=$tag_to_use ./build.sh || exit 1
 popd
 pushd JeMPI_API_KC
-  CI=false TAG=$tag_to_use ./build.sh || exit 1
+  $CI TAG=$tag_to_use ./build.sh || exit 1
 popd
 pushd JeMPI_Bootstrapper
-  CI=false TAG=$tag_to_use ./build.sh || exit 1
+  $CI TAG=$tag_to_use ./build.sh || exit 1
 popd
 pushd JeMPI_BackupRestoreAPI
   ./build.sh || exit 1
