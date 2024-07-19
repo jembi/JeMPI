1a67f5929883e9713b4faa4413ed0a2f5b40e43e
Sushant <sushantpatil1214@gmail.com>
2024-07-19 14:35:15 +0530

    CU-86bzn78dn - Build issue in the Main branch

--- JeMPI_Apps/build-all-ui.sh
+++ JeMPI_Apps/build-all-ui.sh
@@ -2,9 +2,14 @@
 
 set -e
 set -u
+if [ $# -eq 0 ]; then
+    tag_to_use="ci-test-main" 
+else
+    tag_to_use=$1
+fi
 
 pushd JeMPI_UI
-  ./build-image.sh || exit 1
+  CI=false TAG=$tag_to_use ./build-image.sh || exit 1
 popd
 
 pushd JeMPI_UI
