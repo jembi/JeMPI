847b10a35146f9a9dc2254a67746caa0ccb25636
Martin O <martin.oosthuizen@jembi.org>
2024-07-29 14:41:42 +0200

    Prevent caching in docker build

--- JeMPI_Apps/build-app-image.sh
+++ JeMPI_Apps/build-app-image.sh
@@ -7,5 +7,5 @@ pushd ./docker
   export JAR_FILE=$JAR_FILE
   [ -z $(docker images -q ${APP_IMAGE}) ] || docker rmi ${APP_IMAGE}
   docker system prune --volumes -f
-  docker build --build-arg JAVA_VERSION=${JAVA_VERSION_X} --tag $APP_IMAGE .
+  docker build --no-cache --build-arg JAVA_VERSION=${JAVA_VERSION_X} --tag $APP_IMAGE .
 popd
