d38d9c1adbe9e42c766d16fcf50b748715b61542
Matthew Erispe <matthew.erispe@jembi.org>
2024-08-12 14:18:19 +0200

    add ci tag check to backup and restore build

--- JeMPI_Apps/JeMPI_BackupRestoreAPI/build.sh
+++ JeMPI_Apps/JeMPI_BackupRestoreAPI/build.sh
@@ -9,4 +9,9 @@ source ../build-check-jdk.sh
 JAR_FILE=${BACKUP_RESTORE_API_JAR}
 APP_IMAGE=${BACKUP_RESTORE_API_IMAGE}
 APP=backup-restore-api
+
+if [ "$CI" = true ]; then
+    APP_IMAGE=jembi/${ETL_HUB_IMAGE}:${TAG}
+fi
+
 source ../build-app-image.sh
