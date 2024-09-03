bc9a2df075c6a576ce11d9ba30ad4ee486a1bc6d
NyashaMuusha <nyashamuusha@gmail.com>
2024-09-03 10:10:44 +0200

    Updated  formatValue, Typed  data  prop with DataProps

--- JeMPI_Apps/JeMPI_UI/src/components/dashboard/widgets/BetaFscoreWidget.tsx
+++ JeMPI_Apps/JeMPI_UI/src/components/dashboard/widgets/BetaFscoreWidget.tsx
@@ -1,44 +1,46 @@
-import { Box, Grid } from '@mui/material'
-import CountWidget from './CountWidgetWidget'
+import React from 'react';
+import { Box, Grid } from '@mui/material';
+import CountWidget from './CountWidgetWidget';
 
-function BetaFscoreWidget({ data, ...rest }: any) {
+interface DataProps {
+  precision?: number;
+  recall_precision?: number;
+  recall?: number;
+}
+
+interface BetaFscoreWidgetProps {
+  data: DataProps;
+}
+
+function BetaFscoreWidget({ data }: BetaFscoreWidgetProps) {
+  const formatValue = (value: number | undefined): number => 
+    Number.isFinite(value) ? Number(value?.toFixed(5)) : 0;
+
   return (
-    <Box component={'fieldset'}>
+    <Box component="fieldset">
       <legend>Beta F-scores</legend>
       <Grid container spacing={2} columns={16}>
         <Grid item lg={6} xs={16}>
           <CountWidget
             label="Weights precision higher than recall"
-            value={
-              data && typeof data.precision === 'number'
-                ? data.precision.toFixed(5)
-                : ''
-            }
+            value={formatValue(data?.precision)}
           />
         </Grid>
         <Grid item lg={4} xs={16}>
           <CountWidget
             label="Neutral"
-            value={
-              data && typeof data.recall_precision === 'number'
-                ? data.recall_precision.toFixed(5)
-                : ''
-            }
+            value={formatValue(data?.recall_precision)}
           />
         </Grid>
         <Grid item lg={6} xs={16}>
           <CountWidget
             label="Weights recall higher than precision"
-            value={
-              data && typeof data.recall === 'number'
-                ? data.recall.toFixed(5)
-                : ''
-            }
+            value={formatValue(data?.recall)}
           />
         </Grid>
       </Grid>
     </Box>
-  )
+  );
 }
 
-export default BetaFscoreWidget
+export default BetaFscoreWidget;
